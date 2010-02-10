610f97f66110b19a6dc0f364e5b7129bdd704afc
sven <sven@mail.nierlein.de>
2010-02-10 10:57:04 +0100

    updated documentation

--- script/thruk_update_docs.sh
+++ script/thruk_update_docs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+`which asciidoc > /dev/null 2>&1`;
+if [ "$?" -ne "0" ]; then
+    echo "please install the asciidoc package";
+    exit 1;
+fi
+
 cd docs || ( echo "please run from the project root dir"; exit 1; )
-asciidoc --unsafe -a toc -a icons -a data-uri -a max-width=800 INSTALL
-chmod 644 INSTALL.html
+asciidoc --unsafe -a toc -a icons -a data-uri -a max-width=800 THRUK_MANUAL.txt
+chmod 644 THRUK_MANUAL.html
