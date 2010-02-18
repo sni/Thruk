9c8d51b2510d6b167736410ac8b0920f5ee61377
Sven Nierlein <sven@nierlein.de>
2010-02-18 15:56:09 +0100

    fixed local-lib path in scripts

--- script/thruk_fastcgi.pl
+++ script/thruk_fastcgi.pl
@@ -7,7 +7,7 @@ BEGIN {
     use FindBin;
     if(-e "$FindBin::Bin/../local-lib") {
         use lib "$FindBin::Bin/../local-lib/lib/perl5";
-        require local::lib; local::lib->import("$FindBin::Bin/local-lib");
+        require local::lib; local::lib->import("$FindBin::Bin/../local-lib/perl5/");
     }
 }
 
