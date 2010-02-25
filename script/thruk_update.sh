6e3da0bf72efee8d964dbd3b7086617a971501e8
Sven Nierlein <sven@nierlein.de>
2010-02-25 10:31:43 +0100

    fixed update

--- script/thruk_update.sh
+++ script/thruk_update.sh
@@ -8,5 +8,5 @@ fi
 git pull         || ( echo "git pull failed";         exit 1 )
 perl Makefile.PL || ( echo "perl Makefile.PL failed"; exit 1 )
 make             || ( echo "make failed";             exit 1 )
-./contrib/livestatus/patches/create_patched_livestatus_source_dir.sh || ( echo "livcestatus update failed"; exit 1 )
-cd /tmp/livestatus && ./build.sh && ./configure && make && cd -      || ( echo "livcestatus update failed"; exit 1 )
+#./contrib/livestatus/patches/create_patched_livestatus_source_dir.sh || ( echo "livcestatus update failed"; exit 1 )
+#cd /tmp/livestatus && ./build.sh && ./configure && make && cd -      || ( echo "livcestatus update failed"; exit 1 )
