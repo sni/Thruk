79b6134ff1b96ce48db062ce50eaa9cd4f3f203e
Sven Nierlein <sven@nierlein.de>
2010-03-01 08:58:30 +0100

    added possibility to select hosts / services in status detail

--- root/thruk/javascript/thruk.js
+++ root/thruk/javascript/thruk.js
@@ -54,12 +54,11 @@ function stopRefresh() {
   setRefreshRate(0);
 }
 
-
+/* set border color as mouse over for top row buttons*/
 function button_over(button)
 {
    button.style.borderColor = "#555555";
 }
-
 function button_out(button)
 {
    button.style.borderColor = "#999999";
@@ -86,11 +85,175 @@ function toggleBackend(backend) {
     current_backend_states.set(backend, 2);
   }
 
-  /* save current selected backends in cookie */
-  var now         = new Date();
-  var expires     = new Date(now.getTime() + (10*365*86400*1000)); // let the cookie expire in 10 years
-  document.cookie = "thruk_backends="+current_backend_states.toQueryString()+ "; path=/; expires=" + expires.toGMTString() + ";";
+  /* save current selected backends in session cookie */
+  document.cookie = "thruk_backends="+current_backend_states.toQueryString()+ "; path=/;";
   if(initial_state != 3) {
     document.location.reload();
   }
+}
+
+/***************************************
+ * Mouse Over for Status Table
+ * to select hosts / services
+ * for sending quick commands
+ **************************************/
+var selectedServices = new Hash;
+var selectedHosts    = new Hash;
+function addRowSelector(id)
+{
+    var table = document.getElementById(id);
+    var rows  = table.tBodies[0].rows;
+
+    // for each table row, beginning with the second ( dont need table header )
+    for(var row_nr = 1; row_nr < rows.length; row_nr++) {
+        var cells = rows[row_nr].cells;
+
+        // for each cell in a row
+        for(var cell_nr = 0; cell_nr < cells.length; cell_nr++) {
+            if(cell_nr == 0 && cells[0].innerHTML != '') {
+                addEventHandler(cells[cell_nr], 'host');
+            }
+            if(cell_nr >= 1) {
+                addEventHandler(cells[cell_nr], 'service');
+            }
+        }
+    }
+}
+
+/* add the event handler */
+function addEventHandler(elem, type) {
+    if(type == 'host') {
+        elem.onmouseover = highlightHostRow;
+        elem.onmouseout  = resetHostRow;
+        elem.onclick     = selectHost;
+    }
+    if(type == 'service') {
+        elem.onmouseover = highlightServiceRow;
+        elem.onmouseout  = resetServiceRow;
+        elem.onclick     = selectService;
+    }
+}
+
+/* returns the first element which has an id */
+function getFirstParentId(elem) {
+    nr = 0;
+    while(nr < 100 && (!elem.id || elem.id == '')) {
+        nr++;
+        elem = elem.parentNode;
+    }
+    return elem.id;
+}
+
+/* set style for each cell */
+function setRowStyle(row_id, style, type, force) {
+
+    var row = document.getElementById(row_id);
+
+    // for each cells in this row
+    var cells = row.cells;
+    for(var cell_nr = 0; cell_nr < cells.length; cell_nr++) {
+        // only the first cell for hosts
+        // all except the first cell for services
+        if((type == 'host' && cell_nr == 0) || (type == 'service' && cell_nr >= 1)) {
+            // set style for cell itself
+            styleElements(cells[cell_nr], style, force)
+
+            // and for all row elements below
+            var elems = cells[cell_nr].getElementsByTagName('TR');
+            styleElements(elems, style, force)
+
+            // and for all cell elements below
+            var elems = cells[cell_nr].getElementsByTagName('TD');
+            styleElements(elems, style, force)
+        }
+    }
+}
+
+/* save current style and change it*/
+function styleElements(elems, style, force) {
+    if (elems==null || typeof(elems)!="object" || typeof(elems.length)!="number") {
+        elems = new Array(elems);
+    }
+    for(var x = 0; x < elems.length; x++) {
+        if(style == 'original') {
+            // reset style to original
+            if(elems[x].className == "tableRowHover" || force) {
+                elems[x].className = elems[x].origClass;
+            }
+        }
+        else {
+            if(elems[x].className != "tableRowSelected" || force) {
+                // save style in custom attribute
+                if(!elems[x].hasAttribute('origClass')) {
+                    elems[x].setAttribute('origClass', elems[x].className);
+                    elems[x].origClass = elems[x].className;
+                }
+
+                // set new style
+                elems[x].className = style;
+            }
+        }
+    }
+}
+
+/* this is the mouseover function for services */
+function highlightServiceRow(event)
+{
+    // find id of current row
+    var row_id = getFirstParentId(event.target.parentNode);
+    setRowStyle(row_id, 'tableRowHover', 'service');
+    document.title = row_id;
+}
+
+/* this is the mouseover function for hosts */
+function highlightHostRow(event)
+{
+    // find id of current row
+    var row_id = getFirstParentId(event.target.parentNode);
+    setRowStyle(row_id, 'tableRowHover', 'host');
+    document.title = row_id;
+}
+
+/* select this service */
+function selectService(event)
+{
+    // find id of current row
+    var row_id = getFirstParentId(event.target.parentNode);
+    if(selectedServices.get(row_id)) {
+        setRowStyle(row_id, 'original', 'service', true);
+        selectedServices.unset(row_id);
+    } else {
+        setRowStyle(row_id, 'tableRowSelected', 'service', true);
+        selectedServices.set(row_id, 1);
+    }
+}
+
+/* select this host */
+function selectHost(event)
+{
+    // find id of current row
+    var row_id = getFirstParentId(event.target.parentNode);
+    if(selectedHosts.get(row_id)) {
+        setRowStyle(row_id, 'original', 'host', true);
+        selectedHosts.unset(row_id);
+    } else {
+        setRowStyle(row_id, 'tableRowSelected', 'host', true);
+        selectedHosts.set(row_id, 1);
+    }
+}
+
+/* reset row style unless it has been clicked */
+function resetServiceRow(event)
+{
+    // find id of current row
+    var row_id = getFirstParentId(event.target.parentNode);
+    setRowStyle(row_id, 'original', 'service');
+}
+
+/* reset row style unless it has been clicked */
+function resetHostRow(event)
+{
+    // find id of current row
+    var row_id = getFirstParentId(event.target.parentNode);
+    setRowStyle(row_id, 'original', 'host');
 }
