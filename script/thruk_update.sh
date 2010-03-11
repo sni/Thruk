2fd2184ad59dc71427e118be6f5001962aa76c67
Sven Nierlein <sven@nierlein.de>
2010-03-11 11:05:27 +0100

    fixed update script

--- script/thruk_update.sh
+++ script/thruk_update.sh
@@ -5,8 +5,29 @@ if [ ! -d "script" ]; then
   exit 1
 fi
 
-git pull         || ( echo "git pull failed";         exit 1 )
-perl Makefile.PL || ( echo "perl Makefile.PL failed"; exit 1 )
-make             || ( echo "make failed";             exit 1 )
+git pull
+if [ $? != 0 ]; then
+  echo "*** ERROR: git pull failed ***";
+  echo "";
+  echo "please fix git errors first and then run script again";
+  exit 1;
+fi
+
+perl Makefile.PL
+if [ $? != 0 ]; then
+  echo "*** ERROR: perl Makefile.PL failed ***";
+  echo "";
+  echo "please fix errors first";
+  exit 1;
+fi
+
+make
+if [ $? != 0 ]; then
+  echo "*** ERROR: make failed ***";
+  echo "";
+  echo "please fix errors first";
+  exit 1;
+fi
+
 #./contrib/livestatus/patches/create_patched_livestatus_source_dir.sh || ( echo "livcestatus update failed"; exit 1 )
 #cd /tmp/livestatus && ./build.sh && ./configure && make && cd -      || ( echo "livcestatus update failed"; exit 1 )
