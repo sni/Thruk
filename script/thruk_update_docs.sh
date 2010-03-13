9e2ef46ec0b69a2328e420c0e574654e86a039a0
Sven Nierlein <sven@nierlein.de>
2010-03-14 00:51:11 +0100

    updated docs

--- script/thruk_update_docs.sh
+++ script/thruk_update_docs.sh
@@ -7,5 +7,5 @@ if [ "$?" -ne "0" ]; then
 fi
 
 cd docs || ( echo "please run from the project root dir"; exit 1; )
-asciidoc --unsafe -a toc -a icons -a data-uri -a max-width=800 THRUK_MANUAL.txt
+asciidoc --unsafe -a toc -a toclevels=2 -a icons -a data-uri -a max-width=800 THRUK_MANUAL.txt
 chmod 644 THRUK_MANUAL.html
