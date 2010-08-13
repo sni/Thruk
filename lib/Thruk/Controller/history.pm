43019d2f4c37c5cf24f7833d1e75f1523619396f
Sven Nierlein <sven@nierlein.de>
2010-08-13 09:26:21 +0200

    changed history page to db layer

--- lib/Thruk/Controller/history.pm
+++ lib/Thruk/Controller/history.pm
@@ -27,8 +27,8 @@ sub index :Path :Args(0) :MyAction('AddDefaults') {
     my ( $self, $c ) = @_;
 
     my($start,$end);
-    my $filter  = "";
     my $timeframe = 86400;
+    my $filter;
 
     my $oldestfirst = $c->{'request'}->{'parameters'}->{'oldestfirst'} || 0;
     my $archive     = $c->{'request'}->{'parameters'}->{'archive'}     || 0;
@@ -78,80 +78,67 @@ sub index :Path :Args(0) :MyAction('AddDefaults') {
         $end   = $tmp;
     }
 
-    $filter .= "Filter: time >= $start\n";
-    $filter .= "Filter: time <= $end\n";
+    push @{$filter}, { time => { '>=' => $start }};
+    push @{$filter}, { time => { '<=' => $end }};
 
     # type filter
     my $typefilter = $self->_get_log_type_filter($type);
-    if($typefilter ne '') { $typefilter = "\n".$typefilter."And: 2\n"; }
 
     # normal alerts
     my @prop_filter;
     if($statetype == 0) {
-        push @prop_filter, "Filter: type = SERVICE ALERT".$typefilter;
-        push @prop_filter, "Filter: type = HOST ALERT".$typefilter;
+        push @prop_filter, { -and => [{ type => 'SERVICE ALERT'} , $typefilter ]};
+        push @prop_filter, { -and => [{ type => 'HOST ALERT'} , $typefilter ]};
     }
     elsif($statetype == 1) {
-        push @prop_filter, "Filter: type = SERVICE ALERT\nFilter: options ~ ;SOFT;\nAnd: 2".$typefilter;
-        push @prop_filter, "Filter: type = HOST ALERT\nFilter: options ~ ;SOFT;\nAnd: 2".$typefilter;
+        push @prop_filter, { -and => [{ type => 'SERVICE ALERT', options => { '~' => ';SOFT;' }} , $typefilter ]};
+        push @prop_filter, { -and => [{ type => 'HOST ALERT', options => { '~' => ';SOFT;' }} , $typefilter ]};
     }
     if($statetype == 2) {
-        push @prop_filter, "Filter: type = SERVICE ALERT\nFilter: options ~ ;HARD;\nAnd: 2".$typefilter;
-        push @prop_filter, "Filter: type = HOST ALERT\nFilter: options ~ ;HARD;\nAnd: 2".$typefilter;
+        push @prop_filter, { -and => [{ type => 'SERVICE ALERT', options => { '~' => ';HARD;' }} , $typefilter ]};
+        push @prop_filter, { -and => [{ type => 'HOST ALERT', options => { '~' => ';HARD;' }} , $typefilter ]};
     }
 
     # add flapping messages
     unless($noflapping) {
-        push @prop_filter, "Filter: type = SERVICE FLAPPING ALERT";
-        push @prop_filter, "Filter: type = HOST FLAPPING ALERT";
+        push @prop_filter, { type => 'SERVICE FLAPPING ALERT' };
+        push @prop_filter, { type => 'HOST FLAPPING ALERT' };
     }
 
     # add downtime messages
     unless($nodowntime) {
-        push @prop_filter, "Filter: type = SERVICE DOWNTIME ALERT";
-        push @prop_filter, "Filter: type = HOST DOWNTIME ALERT";
-    }
-
-    # join type filter together
-    $filter .= join("\n", @prop_filter)."\nOr: ".(scalar @prop_filter)."\n";
-
-    my $filternum = 0;
-
-    # service filter
-    if(defined $service and $host ne 'all') {
-        $filter .= "Filter: host_name = $host\n";
-        $filter .= "Filter: service_description = $service\n";
-        $filter .= "And: 3\n";
-        $filternum++;
-    }
-    # host filter
-    elsif($host ne 'all') {
-        $filter .= "Filter: host_name = $host\n";
-        $filter .= "And: 2\n";
-        $filternum++;
+        push @prop_filter, { type => 'SERVICE DOWNTIME ALERT' };
+        push @prop_filter, { type => 'HOST DOWNTIME ALERT' };
     }
 
     # add system messages
     unless($nosystem) {
-        $filter .= "Filter: type ~ starting\.\.\.\n";
-        $filter .= "Filter: type ~ shutting\ down\.\.\.\n";
-        $filter .= "Or: 3\n";
+        push @prop_filter, { type => { '~' => 'starting\.\.\.' }};
+        push @prop_filter, { type => { '~' => 'shutting\ down\.\.\.' }};
     }
 
+    # join type filter together
+    push @{$filter}, { -or => \@prop_filter };
 
-    my $query = "GET log\nColumns: time type options state\n".$filter;
-    #$c->log->debug($query);
-    $query   .= Thruk::Utils::Auth::get_auth_filter($c, 'log');
+    # service filter
+    if(defined $service and $host ne 'all') {
+        push @{$filter}, { host_name => $host };
+        push @{$filter}, { service_description => $service };
+    }
+    # host filter
+    elsif($host ne 'all') {
+        push @{$filter}, { host_name => $host };
+    }
 
-    my $logs = $c->{'live'}->selectall_arrayref($query, { Slice => 1, AddPeer => 1});
+    my $total_filter = Thruk::Utils::combine_filter('-and', $filter);
 
     my $order = "DESC";
     if($oldestfirst) {
         $order = "ASC";
     }
-    my $sortedlogs = Thruk::Utils::sort($c, $logs, 'time', $order);
+    my $logs = $c->{'db'}->get_logs(filter => [$total_filter, Thruk::Utils::Auth::get_auth_filter($c, 'log')], sort => {$order => 'time'});
 
-    $c->stash->{logs}             = $sortedlogs;
+    $c->stash->{logs}             = $logs;
     $c->stash->{archive}          = $archive;
     $c->stash->{type}             = $type;
     $c->stash->{statetype}        = $statetype;
@@ -161,8 +148,8 @@ sub index :Path :Args(0) :MyAction('AddDefaults') {
     $c->stash->{oldestfirst}      = $oldestfirst;
     $c->stash->{start}            = $start;
     $c->stash->{end}              = $end;
-    $c->stash->{host}             = $host;
-    $c->stash->{service}          = $service;
+    $c->stash->{host}             = $host    || '';
+    $c->stash->{service}          = $service || '';
     $c->stash->{title}            = 'History';
     $c->stash->{page}             = 'history';
     $c->stash->{template}         = 'history.tt';
@@ -176,47 +163,39 @@ sub _get_log_type_filter {
     my ( $self, $number ) = @_;
 
     $number = 0 if !defined $number or $number <= 0 or $number > 511;
-    my $filter = '';
+    my @prop_filter;
     if($number > 0) {
-        my @prop_filter;
         my @bits = reverse split(/\ */mx, unpack("B*", pack("N", int($number))));
 
         if($bits[0]) {  # 1 - All service alerts
-            push @prop_filter, "Filter: service_description !=";
+            push @prop_filter, { service_description => { '!=' => undef }};
         }
         if($bits[1]) {  # 2 - All host alerts
-            push @prop_filter, "Filter: service_description = ";
+            push @prop_filter, { service_description => undef };
         }
         if($bits[2]) {  # 4 - Service warning
-            push @prop_filter, "Filter: state = 1\nFilter: service_description != \nAnd: 2";
+            push @prop_filter, { state => 1, service_description => { '!=' => undef }};
         }
         if($bits[3]) {  # 8 - Service unknown
-            push @prop_filter, "Filter: state = 3\nFilter: service_description != \nAnd: 2";
+            push @prop_filter, { state => 3, service_description => { '!=' => undef }};
         }
         if($bits[4]) {  # 16 - Service critical
-            push @prop_filter, "Filter: state = 2\nFilter: service_description != \nAnd: 2";
+            push @prop_filter, { state => 2, service_description => { '!=' => undef }};
         }
         if($bits[5]) {  # 32 - Service recovery
-            push @prop_filter, "Filter: state = 0\nFilter: service_description != \nAnd: 2";
+            push @prop_filter, { state => 0, service_description => { '!=' => undef }};
         }
         if($bits[6]) {  # 64 - Host down
-            push @prop_filter, "Filter: state = 1\nFilter: service_description = \nAnd: 2";
+            push @prop_filter, { state => 1, service_description => undef };
         }
         if($bits[7]) {  # 128 - Host unreachable
-            push @prop_filter, "Filter: state = 2\nFilter: service_description = \nAnd: 2";
+            push @prop_filter, { state => 2, service_description => undef };
         }
         if($bits[8]) {  # 256 - Host recovery
-            push @prop_filter, "Filter: state = 0\nFilter: service_description = \nAnd: 2";
-        }
-
-        if(scalar @prop_filter > 1) {
-            $filter .= join("\n", @prop_filter)."\nOr: ".(scalar @prop_filter)."\n";
-        }
-        elsif(scalar @prop_filter == 1) {
-            $filter .= $prop_filter[0]."\n";
+            push @prop_filter, { state => 0, service_description => undef };
         }
     }
-    return($filter);
+    return Thruk::Utils::combine_filter('-or', \@prop_filter);
 }
 
 =head1 AUTHOR
