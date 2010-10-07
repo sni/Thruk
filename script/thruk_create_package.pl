05f83a70f59246432d221af9863c89e9dce65d9e
Sven Nierlein <sven@nierlein.de>
2010-10-07 23:32:56 +0200

    removed modules perldoc from packages

--- script/thruk_create_package.pl
+++ script/thruk_create_package.pl
@@ -84,7 +84,7 @@ elsif(-d "$ENV{'HOME'}/perl5") {
 if(defined $local_lib and $local_lib ne '') {
   print "creating Thruk-".$version."-".$arch.".tar\n";
   cmd("tar zxf $archive");
-  cmd("rsync --modify-window=300 -a $local_lib/ Thruk-".$version."/local-lib");
+  cmd("rsync --modify-window=300 --exclude=man -a $local_lib/ Thruk-".$version."/local-lib");
   cmd("tar cf Thruk-".$version."-".$arch.".tar Thruk-".$version);
   cmd("gzip -9 Thruk-".$version."-".$arch.".tar");
   cmd("rm -rf Thruk-".$version);
@@ -116,12 +116,12 @@ sub cmd {
 sub replace_perl_version {
     my $perl_version = $Config{'version'};
     for my $file (@scripts) {
-        my $content;
-        open(my $fp, '<', $file) or die("cannot open $file: ".$!);
+        my($content, $fp);
+        open($fp, '<', $file) or die("cannot open $file: ".$!);
         while(<$fp>) { $content .= $_; }
         close($fp);
         $content =~ s/###\s+use.*#\s+only\s+required\s+for\s+packages\s+###/use $perl_version;/mx;
-        open(my $fp, '>', $file) or die("cannot write $file: ".$!);
+        open($fp, '>', $file) or die("cannot write $file: ".$!);
         print $fp $content;
         close($fp);
     }
