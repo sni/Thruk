8c7207ed3c0ddf975ffe899cd9c48061dd16ff37
Sven Nierlein <sven@nierlein.org>
2010-10-28 00:07:52 +0200

    fixed Vautour theme css

--- lib/Thruk.pm
+++ lib/Thruk.pm
@@ -36,7 +36,7 @@ use Catalyst qw/
                 Compress::Gzip
                 Thruk::RemoveNastyCharsFromHttpParam
                 /;
-our $VERSION = '0.72';
+our $VERSION = '0.72.1';
 
 ###################################################
 # Configure the application.
@@ -50,7 +50,7 @@ our $VERSION = '0.72';
 my $project_root = __PACKAGE__->config->{home};
 my %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
-              'released'               => 'October 25, 2010',
+              'released'               => 'October 29, 2010',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
               'project_root'           => $project_root,
