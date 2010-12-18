0b5204591b4761509ab7014b948b730480a3e30f
Sven Nierlein <sven@nierlein.de>
2010-12-18 13:46:00 +0100

    release preparation

--- lib/Thruk.pm
+++ lib/Thruk.pm
@@ -50,7 +50,7 @@ our $VERSION = '0.74';
 my $project_root = __PACKAGE__->config->{home};
 my %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
-              'released'               => 'November 12, 2011',
+              'released'               => 'December 18, 2010',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
               'project_root'           => $project_root,
