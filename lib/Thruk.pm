083b0e5dba8f83c6ce81ee96fae2d49f9ac91bec
Sven Nierlein <sven@nierlein.de>
2010-12-26 21:29:24 +0100

    added "link to this page" button on host/status details page

--- lib/Thruk.pm
+++ lib/Thruk.pm
@@ -50,7 +50,7 @@ our $VERSION = '0.76';
 my $project_root = __PACKAGE__->config->{home};
 my %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
-              'released'               => 'January 01, 2011',
+              'released'               => 'December 26, 2010',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
               'project_root'           => $project_root,
