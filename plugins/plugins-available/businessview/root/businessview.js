adf2a87bbb64b44b72753dea0339c281e4c560e2
Gabes Jean <naparuba@gmail.com>
2011-01-10 13:17:12 +0100

    Add : fold/unfold links on all levels.

--- plugins/plugins-available/businessview/root/businessview.js
+++ plugins/plugins-available/businessview/root/businessview.js
@@ -1,17 +1,32 @@
 function toggleToc(e) {
-    alert(e);
-    /*var toc = document.getElementById('toc').getElementsByTagName('ul')[0];*/
+    /*alert(e);*/
     var toc = document.getElementById(e);
-    /*var toggleLink = document.getElementById('togglelink')*/
+    var toggleLink = document.getElementById('togglelink-'+e);
+    var imgLink = document.getElementById('img-'+e);
 
-	if (toc && toc.style.display == 'none') {
-	    toc.style.display = 'block';
-	} else {
-	    toc.style.display = 'none';
-	}
+    if (toc && toc.style.display == 'none') {
+	toc.style.display = 'block';
+	toggleLink.firstChild.nodeValue = 'Fold';
+	imgLink.src = img_src+'go-up.png';
+    } else {
+	toc.style.display = 'none';
+	toggleLink.firstChild.nodeValue = 'Expand';
+	imgLink.src = img_src+'go-next.png';
+    }
+
+    
 }
 
 
 function Affiche(Texte) {
     alert(Texte);
+}
+
+
+function changeText(el, newText) {
+    // Safari work around
+    if (el.innerText)
+        el.innerText = newText;
+    else if (el.firstChild && el.firstChild.nodeValue)
+        el.firstChild.nodeValue = newText;
 }
