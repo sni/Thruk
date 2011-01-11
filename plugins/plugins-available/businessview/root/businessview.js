61c211a97f34f6b8aed91c6ab13f4484755a3702
Gabes Jean <naparuba@gmail.com>
2011-01-11 11:00:25 +0100

    Fix : clean the theme ofthe business view page.

--- plugins/plugins-available/businessview/root/businessview.js
+++ plugins/plugins-available/businessview/root/businessview.js
@@ -1,16 +1,13 @@
 function toggleToc(e) {
     /*alert(e);*/
     var toc = document.getElementById(e);
-    var toggleLink = document.getElementById('togglelink-'+e);
     var imgLink = document.getElementById('img-'+e);
 
     if (toc && toc.style.display == 'none') {
 	toc.style.display = 'block';
-	toggleLink.firstChild.nodeValue = 'Fold';
 	imgLink.src = img_src+'go-up.png';
     } else {
 	toc.style.display = 'none';
-	toggleLink.firstChild.nodeValue = 'Expand';
 	imgLink.src = img_src+'go-next.png';
     }
 
@@ -18,15 +15,3 @@ function toggleToc(e) {
 }
 
 
-function Affiche(Texte) {
-    alert(Texte);
-}
-
-
-function changeText(el, newText) {
-    // Safari work around
-    if (el.innerText)
-        el.innerText = newText;
-    else if (el.firstChild && el.firstChild.nodeValue)
-        el.firstChild.nodeValue = newText;
-}
