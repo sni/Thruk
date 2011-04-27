cd5a461c86fb858e9a098796df012d62fbfd2e65
Sven Nierlein <sven@nierlein.de>
2011-04-27 23:25:36 +0200

    inc version

--- lib/Thruk.pm
+++ lib/Thruk.pm
@@ -36,7 +36,7 @@ use Catalyst qw/
                 Compress::Gzip
                 Thruk::RemoveNastyCharsFromHttpParam
                 /;
-our $VERSION = '0.94.1';
+our $VERSION = '0.94.2';
 
 ###################################################
 # Configure the application.
@@ -50,7 +50,7 @@ our $VERSION = '0.94.1';
 my $project_root = __PACKAGE__->config->{home};
 my %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
-              'released'               => 'April 26, 2011',
+              'released'               => 'April 28, 2011',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
               'project_root'           => $project_root,
