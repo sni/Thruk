78cb1564bdc4507d804fbd46072cc53fef9322ff
Sven Nierlein <sven@nierlein.de>
2011-08-02 16:22:17 +0200

    updated documentation

--- lib/Thruk.pm
+++ lib/Thruk.pm
@@ -50,7 +50,7 @@ our $VERSION = '1.0.8';
 my $project_root = __PACKAGE__->config->{home};
 my %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
-              'released'               => 'August 01, 2011',
+              'released'               => 'August 02, 2011',
               compression_format       => 'gzip',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
