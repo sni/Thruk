260ed453c8ecead4475cbecc567bcfe19bfb0eeb
Sven Nierlein <sven@nierlein.de>
2011-12-26 23:43:50 +0100

    mobile: fixed config button layout

--- lib/Thruk/Utils.pm
+++ lib/Thruk/Utils.pm
@@ -958,7 +958,7 @@ sub choose_mobile {
     my($c,$url) = @_;
 
     return unless defined $c->{'request'}->{'headers'}->{'user-agent'};
-    return unless $c->{'request'}->{'headers'}->{'user-agent'} =~ m/(iPhone|Android|)/mx;
+    return unless $c->{'request'}->{'headers'}->{'user-agent'} =~ m/(iPhone|Android)/mx;
 
     my $choose_mobile;
     if(defined $c->request->cookie('thruk_mobile')) {
