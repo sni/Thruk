7ea9f1af9c63f4617776dc8db1baf21a1c87eee6
Sven Nierlein <sven@nierlein.de>
2012-03-29 16:32:14 +0200

    run dos2unix for combined js files

--- script/thruk_create_combined_static_content.pl
+++ script/thruk_create_combined_static_content.pl
@@ -4,8 +4,12 @@
 use lib 'lib';
 use Thruk;
 
+my $dos2unix = "/usr/bin/dos2unix";
+$dos2unix = "/usr/bin/fromdos" if -x "/usr/bin/fromdos";
+
 my $cmds = [
     'cd root/thruk/javascript && cat '.join(' ', @{Thruk->config->{'View::TT'}->{'PRE_DEFINE'}->{'all_in_one_javascript'}}).' > all_in_one-'.$Thruk::VERSION.'.js',
+    'cd root/thruk/javascript && '.$dos2unix.' all_in_one-'.$Thruk::VERSION.'.js',
     'cd themes/themes-available/Thruk/stylesheets/ && cat '.join(' ', @{Thruk->config->{'View::TT'}->{'PRE_DEFINE'}->{'all_in_one_css_noframes'}}).' > all_in_one_noframes-'.$Thruk::VERSION.'.css',
     'cd themes/themes-available/Thruk/stylesheets/ && cat '.join(' ', @{Thruk->config->{'View::TT'}->{'PRE_DEFINE'}->{'all_in_one_css_frames'}}).' > all_in_one-'.$Thruk::VERSION.'.css',
 ];
