56936060335a01f93d7548efbbb78e099e74c578
Sven Nierlein <sven@nierlein.de>
2012-04-29 20:46:02 +0200

    starting new version

--- lib/Thruk.pm
+++ lib/Thruk.pm
@@ -41,7 +41,7 @@ use Catalyst qw/
                 Cache
                 Thruk::RemoveNastyCharsFromHttpParam
                 /;
-our $VERSION = '1.28';
+our $VERSION = '1.29';
 
 ###################################################
 # Configure the application.
@@ -62,7 +62,7 @@ if(-d $project_root.'/.git') {
 }
 my %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
-              'released'               => 'April 28, 2012',
+              'released'               => 'April 29, 2012',
               'compression_format'     => 'gzip',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
--- support/thruk.spec
+++ support/thruk.spec
@@ -1,5 +1,5 @@
 Name:          thruk
-Version:       1.28
+Version:       1.29
 Release:       1%{?dist}
 License:       GNU Public License version 2
 Packager:      Sven Nierlein <sven.nierlein@consol.de>
