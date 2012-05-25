f184eb29a5036cde4afbf27d0e66735196f4cc38
Sven Nierlein <sven@nierlein.de>
2012-05-25 14:38:22 +0200

    load system defaults for fcgid too

--- support/fcgid_env.sh
+++ support/fcgid_env.sh
@@ -3,5 +3,10 @@
 # set omd environment
 export CATALYST_CONFIG="/etc/thruk"
 
+# load extra environment variables
+if [ -f /etc/sysconfig/thruk ]; then
+  . /etc/sysconfig/thruk
+fi
+
 # execute fastcgi server
 exec /usr/share/thruk/script/thruk_fastcgi.pl
