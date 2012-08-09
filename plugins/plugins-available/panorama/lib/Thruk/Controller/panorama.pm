cc11cb8656d3ec2e65fb097a7327d6745cfe2575
Sven Nierlein <sven@nierlein.de>
2012-08-09 19:30:26 +0200

    panorama: link service details to service list

--- plugins/plugins-available/panorama/lib/Thruk/Controller/panorama.pm
+++ plugins/plugins-available/panorama/lib/Thruk/Controller/panorama.pm
@@ -545,9 +545,11 @@ sub _task_services {
 
     my $json = {
         columns => [
-            { 'header' => 'Hostname',               width => 120, dataIndex => 'host_display_name', renderer => 'TP.render_service_host' },
+            { 'header' => 'Hostname',               width => 120, dataIndex => 'host_display_name',                renderer => 'TP.render_service_host' },
+            { 'header' => 'Host',                                 dataIndex => 'host_name',     hidden => JSON::XS::true },
             { 'header' => 'Host Icons',             width => 75,  dataIndex => 'icons',         align => 'right',  renderer => 'TP.render_host_service_icons' },
-            { 'header' => 'Service',                width => 120, dataIndex => 'display_name' },
+            { 'header' => 'Service',                width => 120, dataIndex => 'display_name',                     renderer => 'TP.render_clickable_service' },
+            { 'header' => 'Description',                          dataIndex => 'description',   hidden => JSON::XS::true },
             { 'header' => 'Icons',                  width => 75,  dataIndex => 'icons',         align => 'right',  renderer => 'TP.render_service_icons' },
             { 'header' => 'Status',                 width => 70,  dataIndex => 'state',         align => 'center', renderer => 'TP.render_service_status' },
             { 'header' => 'Last Check',             width => 80,  dataIndex => 'last_check',    align => 'center', renderer => 'TP.render_last_check' },
