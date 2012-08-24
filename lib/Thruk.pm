b2bcbc19524a32df534d64af58e9562dda323000
Sven Nierlein <sven@nierlein.de>
2012-08-24 13:47:58 +0200

    starting new version...

--- lib/Thruk.pm
+++ lib/Thruk.pm
@@ -55,7 +55,7 @@ use Catalyst qw/
                 Cache
                 Thruk::RemoveNastyCharsFromHttpParam
                 /;
-our $VERSION = '1.42';
+our $VERSION = '1.43';
 
 ###################################################
 # Configure the application.
@@ -72,7 +72,7 @@ my $gitbranch    = Thruk::Utils::get_git_name($project_root);
 $branch          = $gitbranch unless $branch ne '';
 my %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
-              'released'               => 'August 23, 2012',
+              'released'               => 'August 24, 2012',
               'compression_format'     => 'gzip',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
