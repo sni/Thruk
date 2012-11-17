36fb16be45ef2c1bf57faee195bcc7886c3722bd
Sven Nierlein <sven@nierlein.org>
2012-11-17 16:59:08 +0100

    only update docs when source file is newer

--- script/thruk_update_docs.sh
+++ script/thruk_update_docs.sh
@@ -10,24 +10,26 @@ DOS2UNIX=$(which dos2unix || which fromdos)
 
 cd docs || ( echo "please run from the project root dir"; exit 1; )
 
-if [ $(git status | grep THRUK_MANUAL.txt | wc -l) -gt 0 ]; then
+if [ THRUK_MANUAL.txt -nt THRUK_MANUAL.html ]; then
     asciidoc --unsafe -a toc -a toclevels=2 -a icons -a data-uri -a max-width=800 THRUK_MANUAL.txt
     chmod 644 THRUK_MANUAL.html
     $DOS2UNIX THRUK_MANUAL.html
 fi
 
-if [ $(git status | grep FAQ.txt | wc -l) -gt 0 ]; then
+if [ FAQ.txt -nt FAQ.html ]; then
     asciidoc --unsafe -a toc -a toclevels=2 -a icons -a data-uri -a max-width=800 FAQ.txt
     chmod 644 FAQ.html
     $DOS2UNIX FAQ.html
 fi
 
 
-if [ $(git status | grep thruk8.txt | wc -l) -gt 0 ]; then
+if [ thruk8.txt -nt thruk.8 ]; then
     a2x -d manpage -f manpage thruk8.txt
     chmod 644 thruk.8
     $DOS2UNIX thruk.8
 fi
 
-pod2man -s 3 -n thruk   ../script/thruk   > thruk.3
-pod2man -s 3 -n naglint ../script/naglint > naglint.3
+[ thruk   -nt thruk.3 ]   && pod2man -s 3 -n thruk   ../script/thruk   > thruk.3
+[ naglint -nt naglint.3 ] && pod2man -s 3 -n naglint ../script/naglint > naglint.3
+
+exit
