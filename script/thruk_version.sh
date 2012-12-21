2abf15890e1061eb6e4462426a73c3568b836336
Sven Nierlein <sven@nierlein.de>
2012-12-21 19:37:53 +0100

    code cleanup in make newversion

--- script/thruk_version.sh
+++ script/thruk_version.sh
@@ -40,19 +40,29 @@ fi
 sed -r "s/'released'\s*=>\s*'.*',/'released'               => '$date',/" -i lib/Thruk/Config.pm
 sed -i support/thruk.spec -e 's/^Release:.*$/Release: '$rpmrelease'%{?dist}/'
 if [ "$branch" != "" ]; then
-    sed -r "s/branch\s*= '';/branch = '$branch';/"
-        -i lib/Thruk/Config.pm
-        -i script/thruk
-        -i script/naglint
-        -i script/nagexp
+    sed -r "s/branch\s*= '';/branch = '$branch';/" \
+        -i lib/Thruk/Config.pm \
+        -i script/thruk        \
+        -i script/naglint      \
+        -i script/nagexp       \
         -i script/nagimp
 fi
 dch --newversion "$debversion" --package "thruk" -D "UNRELEASED" "new upstream release"
 if [ -n "$newversion" -a "$newversion" != "$VERSION" ]; then
     sed -r "s/Version:\s*$VERSION/Version:       $newversion/" -i support/thruk.spec
-    sed -r "s/'$VERSION'/'$newversion'/" -i lib/Thruk.pm -i lib/Thruk/Config.pm -i support/thruk.spec -i script/thruk -i script/naglint -i script/nagexp -i script/nagimp
-    sed -r "s/_$VERSION_/_$newversion\_/" -i docs/THRUK_MANUAL.txt
-    sed -r "s/\-$VERSION\./-$newversion\./" -i MANIFEST -i docs/THRUK_MANUAL.txt -i root/thruk/startup.html
+    sed -r "s/'$VERSION'/'$newversion'/" \
+                -i lib/Thruk.pm          \
+                -i lib/Thruk/Config.pm   \
+                -i support/thruk.spec    \
+                -i script/thruk          \
+                -i script/naglint        \
+                -i script/nagexp         \
+                -i script/nagimp
+    sed -r "s/_"$VERSION"_/_$newversion\_/" -i docs/THRUK_MANUAL.txt
+    sed -r "s/\-$VERSION\./-$newversion\./" \
+                -i MANIFEST              \
+                -i docs/THRUK_MANUAL.txt \
+                -i root/thruk/startup.html
     sed -r "s/\-$VERSION\-/-$newversion\-/" -i docs/THRUK_MANUAL.txt
     sed -r "s/$VERSION\s*not yet released/$newversion     $fulldate/" -i Changes
     sed -r "s/$VERSION/$newversion/" -i dist.ini
@@ -66,6 +76,19 @@ if [ -n "$newversion" -a "$newversion" != "$VERSION" ]; then
 fi
 ./script/thruk_update_docs.sh > /dev/null
 yes n | perl Makefile.PL > /dev/null
-git add MANIFEST support/thruk.spec docs/THRUK_MANUAL.txt docs/THRUK_MANUAL.html lib/Thruk.pm docs/thruk.3 root/thruk/startup.html script/thruk dist.ini lib/Thruk/Config.pm script/naglint script/nagexp script/nagimp
+git add                     \
+    MANIFEST                \
+    support/thruk.spec      \
+    docs/THRUK_MANUAL.txt   \
+    docs/THRUK_MANUAL.html  \
+    lib/Thruk.pm            \
+    docs/thruk.3            \
+    root/thruk/startup.html \
+    script/thruk            \
+    dist.ini                \
+    lib/Thruk/Config.pm     \
+    script/naglint          \
+    script/nagexp           \
+    script/nagimp
 git co docs/FAQ.html
 git status
