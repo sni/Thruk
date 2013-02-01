c07909c043e5445a37f0ff8bbfd34515c4f9ed4f
Sven Nierlein <sven@nierlein.de>
2013-02-01 15:52:21 +0100

    no stacktrace for timeouts

--- lib/Thruk/Backend/Manager.pm
+++ lib/Thruk/Backend/Manager.pm
@@ -887,6 +887,9 @@ sub _do_on_peers {
         }
         elsif($err =~ m|(failed\s+to\s+open\s+socket\s+[^:]+:.*?)\s+at\s+|mx) {
             die($1);
+        }
+        elsif($err =~ m|(hit\s\+.*?timeout\s+on.*?)\s+at\s+|mx) {
+            die($1);
         } else {
             local $Data::Dumper::Deepcopy = 1;
             confess("Error in _do_on_peers: ".$err."called as ".Dumper($function)."with args: ".Dumper(\%arg));
