c3c2c0bab0ea9c6982458632725e00791d8aa23b
Sven Nierlein <sven@nierlein.de>
2013-03-14 21:34:48 +0100

    removed background color from status page (can be reenabled by 'status_color_background')

--- lib/Thruk/Config.pm
+++ lib/Thruk/Config.pm
@@ -392,6 +392,7 @@ sub set_default_config {
         'precompile_templates'              => 1,
         'report_use_temp_files'             => 14,
         'perf_bar_pnp_popup'                => 1,
+        'status_color_background'           => 0,
     };
     $defaults->{'thruk_bin'} = 'script/thruk' if -f 'script/thruk';
     for my $key (keys %{$defaults}) {
--- lib/Thruk/Controller/Root.pm
+++ lib/Thruk/Controller/Root.pm
@@ -51,6 +51,7 @@ sub begin : Private {
                   show_backends_in_table host_action_icon service_action_icon cookie_path
                   use_feature_trends show_error_reports skip_js_errors perf_bar_mode
                   bug_email_rcpt home_link first_day_of_week sitepanel perf_bar_pnp_popup
+                  status_color_background
                 /) {
         $c->stash->{$key} = $c->config->{$key};
     }
