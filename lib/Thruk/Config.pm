9ce46437370beb69de563ce5bdf21e60dfd7920e
Sven Nierlein <Sven.Nierlein@consol.de>
2013-05-06 20:48:14 +0200

    released 1.70

--- lib/Thruk/Config.pm
+++ lib/Thruk/Config.pm
@@ -20,7 +20,7 @@ Generic Access to Thruks Config
 
 ######################################
 
-our $VERSION = '1.68';
+our $VERSION = '1.70';
 
 my $project_root = Catalyst::Utils::home('Thruk::Config');
 my $branch       = '';
@@ -30,7 +30,7 @@ $branch          = $gitbranch unless $branch ne '';
 our %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
               'branch'                 => $branch,
-              'released'               => 'April 09, 2013',
+              'released'               => 'May 06, 2013',
               'compression_format'     => 'gzip',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
