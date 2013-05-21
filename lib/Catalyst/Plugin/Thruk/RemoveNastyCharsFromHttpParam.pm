d6ff88c64acf2598e6f459bc333f06f15aa3ce98
Sven Nierlein <sven@nierlein.de>
2013-05-21 11:39:28 +0200

    don't strip nasty chars in passwords

--- lib/Catalyst/Plugin/Thruk/RemoveNastyCharsFromHttpParam.pm
+++ lib/Catalyst/Plugin/Thruk/RemoveNastyCharsFromHttpParam.pm
@@ -27,6 +27,7 @@ sub prepare_uploads {
         next if $key eq 'content';
         next if $key eq 'filter';
         next if $key eq 'performance_data';
+        next if $key eq 'password';
         next if $key =~ /^s\d+_op/mx;
         next if $key =~ /^s\d+_value/mx;
         next if $key =~ /^\w{3}_s\d+_value/mx;
