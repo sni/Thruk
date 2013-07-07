a2e2480b074153a422937c318f48eb47f6882dec
awiddersheim <awiddersheim@inetu.net>
2013-07-07 14:03:16 +0200

    More detailed page titles
    Wanted to have more detailed page titles which is helpful when you have a lot of tabs open in your browser. You can find the page you are looking for easier and quicker.

--- lib/Thruk/Controller/extinfo.pm
+++ lib/Thruk/Controller/extinfo.pm
@@ -32,6 +32,7 @@ sub index : Path : Args(0) : MyAction('AddDefaults') {
     $c->stash->{template}     = 'extinfo_type_' . $type . '.tt';
 
     my $infoBoxTitle;
+    my $title = undef;
     if( $type == 0 ) {
         $infoBoxTitle = 'Process Information';
         return $c->detach('/error/index/1') unless $c->check_user_roles("authorized_for_system_information");
@@ -39,10 +40,12 @@ sub index : Path : Args(0) : MyAction('AddDefaults') {
     }
     if( $type == 1 ) {
         $infoBoxTitle = 'Host Information';
+        $title = $c->{'request'}->{'parameters'}->{'host'};
         return unless $self->_process_host_page($c);
     }
     if( $type == 2 ) {
         $infoBoxTitle = 'Service Information';
+        $title = $c->{'request'}->{'parameters'}->{'service'} . " @ " . $c->{'request'}->{'parameters'}->{'host'};
         return unless $self->_process_service_page($c);
     }
     if( $type == 3 ) {
@@ -55,6 +58,7 @@ sub index : Path : Args(0) : MyAction('AddDefaults') {
     }
     if( $type == 5 ) {
         $infoBoxTitle = 'Hostgroup Information';
+        $title = $c->{'request'}->{'parameters'}->{'hostgroup'} . " " . $infoBoxTitle;
         $self->_process_hostgroup_cmd_page($c);
     }
     if( $type == 6 ) {
@@ -72,7 +76,15 @@ sub index : Path : Args(0) : MyAction('AddDefaults') {
     }
     if( $type == 8 ) {
         $infoBoxTitle = 'Servicegroup Information';
+        $title = $c->{'request'}->{'parameters'}->{'servicegroup'} . " " . $infoBoxTitle;
         $self->_process_servicegroup_cmd_page($c);
+    }
+
+    if(defined($title)) {
+        $c->stash->{title} = $title;
+    }
+    elsif(defined($infoBoxTitle)) {
+        $c->stash->{title} = $infoBoxTitle;
     }
 
     $c->stash->{infoBoxTitle} = $infoBoxTitle;
