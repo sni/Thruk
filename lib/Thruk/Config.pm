5e97825d629f5a6f0f83f3e60f04a7aa9d7c735d
Sven Nierlein <Sven.Nierlein@consol.de>
2013-08-25 19:09:21 +0200

    released 1.76-2

--- lib/Thruk/Config.pm
+++ lib/Thruk/Config.pm
@@ -29,7 +29,7 @@ $branch          = $gitbranch unless $branch ne '';
 our %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
               'branch'                 => $branch,
-              'released'               => 'August 23, 2013',
+              'released'               => 'August 25, 2013',
               'compression_format'     => 'gzip',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
