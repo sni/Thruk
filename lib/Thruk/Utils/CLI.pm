b36ec6ab391d302db4dd586e267b4e46723e7227
Sven Nierlein <sven@nierlein.de>
2014-05-14 07:57:04 +0200

    unlink cache and token on updates

--- lib/Thruk/Utils/CLI.pm
+++ lib/Thruk/Utils/CLI.pm
@@ -561,7 +561,8 @@ sub _run_command_action {
     }
     elsif($action eq 'clearcache') {
         $data->{'rc'} = 0;
-        $c->cache->clear;
+        unlink($c->config->{'var_path'}.'/token');
+        unlink($c->config->{'tmp_path'}.'/thruk.cache');
         $data->{'output'} = "cache cleared";
     }
 
