3d4fd29a9a4705e88a2e55029eb1d58f882d81bb
Sven Nierlein <sven@nierlein.de>
2014-06-22 20:10:25 +0200

    tests: skip external pnp links

--- t/TestUtils.pm
+++ t/TestUtils.pm
@@ -373,6 +373,7 @@ sub test_page {
         my $errors = 0;
         for my $test_url (keys %{$links_to_check}) {
             next if $test_url =~ m/\/pnp4nagios\//mxo;
+            next if $test_url =~ m/\/pnp\//mxo;
             next if $test_url =~ m|/$product/themes/.*?/images/logos/|mxo;
             if($test_url !~ m/^(http|\/)/gmxo) { $test_url = _relative_url($test_url, $request->base()->as_string()); }
             my $request = _request($test_url, undef, undef, $opts->{'agent'});
