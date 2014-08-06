02de36f2223977040d683b59d2ecd37ff0900696
Sven Nierlein <sven@nierlein.de>
2014-08-06 12:59:06 +0200

    // operator is not available on centos 5

--- lib/Thruk/Utils/Status.pm
+++ lib/Thruk/Utils/Status.pm
@@ -127,10 +127,10 @@ sub get_search_from_param {
         if( ref $c->{'request'}->{'parameters'}->{ $prefix . '_type' } eq 'ARRAY' ) {
             for ( my $x = 0; $x < scalar @{ $c->{'request'}->{'parameters'}->{ $prefix . '_type' } }; $x++ ) {
                 my $text_filter = {
-                    val_pre => $c->{'request'}->{'parameters'}->{ $prefix . '_val_pre' }->[$x] // '',
-                    type    => $c->{'request'}->{'parameters'}->{ $prefix . '_type' }->[$x]    // '',
-                    value   => $c->{'request'}->{'parameters'}->{ $prefix . '_value' }->[$x]   // '',
-                    op      => $c->{'request'}->{'parameters'}->{ $prefix . '_op' }->[$x]      // '',
+                    val_pre => is_defined($c->{'request'}->{'parameters'}->{ $prefix . '_val_pre' }->[$x], ''),
+                    type    => is_defined($c->{'request'}->{'parameters'}->{ $prefix . '_type' }->[$x],    ''),
+                    value   => is_defined($c->{'request'}->{'parameters'}->{ $prefix . '_value' }->[$x],   ''),
+                    op      => is_defined($c->{'request'}->{'parameters'}->{ $prefix . '_op' }->[$x],      ''),
                 };
                 if($text_filter->{'type'} eq 'business impact' and defined $c->{'request'}->{'parameters'}->{ $prefix . '_value_sel' }->[$x]) {
                     $text_filter->{'value'} = $c->{'request'}->{'parameters'}->{ $prefix . '_value_sel' }->[$x];
@@ -141,10 +141,10 @@ sub get_search_from_param {
         }
         else {
             my $text_filter = {
-                val_pre => $c->{'request'}->{'parameters'}->{ $prefix . '_val_pre' } // '',
-                type    => $c->{'request'}->{'parameters'}->{ $prefix . '_type' }    // '',
-                value   => $c->{'request'}->{'parameters'}->{ $prefix . '_value' }   // '',
-                op      => $c->{'request'}->{'parameters'}->{ $prefix . '_op' }      // '',
+                val_pre => is_defined($c->{'request'}->{'parameters'}->{ $prefix . '_val_pre' }, ''),
+                type    => is_defined($c->{'request'}->{'parameters'}->{ $prefix . '_type' },    ''),
+                value   => is_defined($c->{'request'}->{'parameters'}->{ $prefix . '_value' },   ''),
+                op      => is_defined($c->{'request'}->{'parameters'}->{ $prefix . '_op' },      ''),
             };
             if(defined $c->{'request'}->{'parameters'}->{ $prefix . '_value_sel'} and $text_filter->{'type'} eq 'business impact') {
                 $text_filter->{'value'} = $c->{'request'}->{'parameters'}->{ $prefix . '_value_sel'};
@@ -1891,6 +1891,12 @@ sub get_service_matrix {
     return($uniq_services, $hosts, $matrix);
 }
 
+##############################################
+sub is_defined {
+    my($a, $b) = @_;
+    return $a if defined $a;
+    return $b;
+}
 
 ##############################################
 
