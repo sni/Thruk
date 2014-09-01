61aba8890922155ac3334913c237de280ac190e3
Sven Nierlein <sven@nierlein.de>
2014-09-01 21:54:50 +0200

    added bp to test pages
    Signed-off-by: Sven Nierlein <sven@nierlein.de>

--- script/test_page_rps.sh
+++ script/test_page_rps.sh
@@ -3,54 +3,88 @@
 set -e
 set -u
 
+#################################################
+# settings
 NUM=10
 REQUESTS=100
 CONCURRENCY=5
 DELAY=3
+BASEPORT="3000"
+BASEURL="http://127.0.0.1:$BASEPORT/thruk"
 
-# clean up
+#################################################
 cleanup() {
   ps -efl | grep thruk_server | grep perl | awk '{ print $4 }' | xargs -r kill >/dev/null 2>&1
 }
 
-trap "cleanup" EXIT
-cleanup
+#################################################
+test_page() {
+  NAME=$1
+  URL=$2
+  pageres=$(ab -c $CONCURRENCY -n $REQUESTS $URL 2>&1)
+  page=$(echo "$pageres" | grep 'Requests per second:' | awk '{ print $4 }')
+  pageerr=$(echo "$pageres" | grep 'Non-2xx responses:' | awk '{ print $3 }')
+  if [ "$pageerr" != "" ]; then if [ $pageerr -gt 5 ]; then page='err'; fi; fi
+  printf "     %s: %5s #/sec" "$NAME" "$page"
+  sleep $DELAY
+}
 
-for tag in integration master $(git tag -l | tail -$NUM | tac); do
-  printf "%-12s  " "$tag"
-  git checkout $tag > /dev/null 2>&1
+#################################################
+switch_tag() {
+  TAG="$1"
+  git=$(git checkout $TAG 2>&1 || echo -n)
+  if [[ $git == *error:* ]]; then
+    printf "\n$git"
+    exit
+  fi
+}
+
+#################################################
+test_tag() {
+  TAG="$1"
+  printf "%-8s  " "$TAG"
+  switch_tag "$TAG"
   pid=$(./script/thruk_server.pl >/dev/null 2>&1 & echo $!);
   sleep $DELAY
-  while [ $(lsof -i:3000 | grep -c LISTEN) -ne 1 ]; do
+  while [ $(lsof -i:$BASEPORT | grep -c LISTEN) -ne 1 ]; do
     sleep 0.5
     ps -p $pid >/dev/null 2>&1 || { echo "failed to start!"; exit; }
   done
 
   # warm up
-  ab -c $CONCURRENCY -n 10 http://127.0.0.1:3000/thruk/cgi-bin/tac.cgi > /dev/null 2>&1
+  ab -c $CONCURRENCY -n 10 "$BASEURL/cgi-bin/tac.cgi" > /dev/null 2>&1
   sleep $DELAY
 
-  tacres=$(ab -c $CONCURRENCY -n $REQUESTS http://127.0.0.1:3000/thruk/cgi-bin/tac.cgi 2>&1)
-  tac=$(echo "$tacres" | grep 'Requests per second:' | awk '{ print $4 }')
-  tacerr=$(echo "$tacres" | grep 'Non-2xx responses:' | awk '{ print $3 }')
-  if [ "$tacerr" != "" ]; then if [ $tacerr -gt 5 ]; then tac='err'; fi; fi
-
-  stares=$(ab -c $CONCURRENCY -n $REQUESTS http://127.0.0.1:3000/thruk/cgi-bin/status.cgi 2>&1)
-  sta=$(echo "$stares" | grep 'Requests per second:' | awk '{ print $4 }')
-  staerr=$(echo "$stares" | grep 'Non-2xx responses:' | awk '{ print $3 }')
-  if [ "$staerr" != "" ]; then if [ $staerr -gt 5 ]; then sta='err'; fi; fi
-
-  jsonres=$(ab -c $CONCURRENCY -n $REQUESTS 'http://127.0.0.1:3000/thruk/cgi-bin/status.cgi?style=hostdetail&hostgroup=all&view_mode=json' 2>&1)
-  json=$(echo "$jsonres" | grep 'Requests per second:' | awk '{ print $4 }')
-  jsonerr=$(echo "$jsonres" | grep 'Non-2xx responses:' | awk '{ print $3 }')
-  if [ "$jsonerr" != "" ]; then if [ $jsonerr -gt 5 ]; then json='err'; fi; fi
+  test_page 'tac'    "$BASEURL/cgi-bin/tac.cgi"
+  test_page 'status' "$BASEURL/cgi-bin/status.cgi"
+  test_page 'json'   "$BASEURL/cgi-bin/status.cgi?style=hostdetail&hostgroup=all&view_mode=json"
+  test_page 'bp'     "$BASEURL/cgi-bin/bp.cgi"
 
   mem=$(ps -efl | grep "./script/thruk_server.pl" | grep -v 'grep' | awk '{print $10}')
+  load=$(w | grep load | awk '{ print $11 }' | tr -d ,)
 
   kill $pid
-  printf "mem: %3d MB    tac: %s #/sec    status: %s #/sec    json: %s #/sec\n" $(echo $mem/1000|bc) "$tac" "$sta" "$json"
+  printf "     mem: %3d MB     load: %5s\n" $(echo $mem/1000|bc) "$load"
   sleep $DELAY
+}
+
+#################################################
+# prepare
+cleanup
+trap "cleanup" EXIT
+
+#################################################
+# run tests
+tags=$(git tag -l | awk -F- '{ print $1 }' | sort -u | tail -$NUM | tac)
+for tag in master $tags; do
+  if [[ $tag == v* ]]; then
+    # get latest sp for this tag
+    tag=$(git tag -l | grep $tag | tail -n 1)
+  fi
+  test_tag "$tag"
 done
 
+#################################################
+# clean up
 cleanup
-git checkout master >/dev/null 2>&1
+switch_tag "master"
