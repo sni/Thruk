2c43f104e06dac5a98961f800d5f6b973ecd6181
Sven Nierlein <sven@nierlein.de>
2014-09-02 18:43:47 +0200

    only print io info if there is such

--- lib/Thruk.pm
+++ lib/Thruk.pm
@@ -412,7 +412,14 @@ after finalize => sub {
         $url     = $c->request->uri unless $url;
         $url     =~ s/^cgi\-bin\///mxo;
         if(length($url) > 50) { $url = substr($url, 0, 50).'...' }
-        $c->log->info(sprintf("Req: %03d, mem:% 7s MB  % 10.2f MB     %.2fs (%.2fs)    %s\n", ($Catalyst::COUNT || 0), $c->stash->{'memory_end'}, ($c->stash->{'memory_end'}-$c->stash->{'memory_begin'}), $elapsed, ($c->stash->{'total_backend_waited'} || 0), $url));
+        $c->log->info(sprintf("Req: %03d, mem:% 7s MB  % 10.2f MB     %.2fs %8s    %s\n",
+                                ($Catalyst::COUNT || 0),
+                                $c->stash->{'memory_end'},
+                                ($c->stash->{'memory_end'}-$c->stash->{'memory_begin'}),
+                                $elapsed,
+                                defined $c->stash->{'total_backend_waited'} ? sprintf('(%.2fs)', $c->stash->{'total_backend_waited'}) : '',
+                                $url,
+                    ));
     }
 
     if($ENV{'THRUK_PERFORMANCE_DEBUG'} and $ENV{'THRUK_PERFORMANCE_DEBUG'} >= 2) {
