bb90022e900b15ba13ddaf4f0da7c0e0de055e19
Sven Nierlein <sven@nierlein.de>
2014-09-15 13:11:00 +0200

    print column headers in timer
    Signed-off-by: Sven Nierlein <sven@nierlein.de>

--- lib/Thruk/Timer.pm
+++ lib/Thruk/Timer.pm
@@ -12,40 +12,42 @@ my $lasttime    = $starttime;
 my $lastmemory  = 0;
 my $has_threads = 0;
 my $has_memory  = 1;
+
+printf(STDERR "%-8s  %7s    %7s    %7s   %8s    %-50s %s\n",
+            "thread", "ttime", "dtime", "tmem", "dmem", "message", "caller");
+printf(STDERR ("-"x140)."\n");
+
 sub timing_breakpoint {
-    my($msg) = @_;
-    my @caller = caller;
-    my $tmp = [gettimeofday];
-    my $elapsed;
-    if($lasttime) {
-        my $total    = tv_interval($starttime);
-        $elapsed     = tv_interval($lasttime);
-        my $memory   = '';
-        my $deltamem = '';
-        if($has_memory) {
-            my $status   = read_file('/proc/'.$$.'/status');
-            $status     =~ m/^VmRSS:\s*(\d+)\s+kB/smxo;
-            $memory     = $1;
-            $deltamem   = $memory - $lastmemory;
-            $lastmemory = $memory;
-        }
-        my $thr    = 'global';
-        if($has_threads) {
-            $thr = (threads->tid()||'global');
-        }
-        my $callfile = $caller[1];
-        $callfile =~ s|^.*lib/||mxo;
-        printf(STDERR "%-8s  %7s    %7s    %8s   %8s    %-50s %s:%d\n",
-                        $thr,
-                        $elapsed > 0.001 ? sprintf("%.3fs", $total)    : '------',
-                        $elapsed > 0.001 ? sprintf("%.3fs", $elapsed) : '------',
-                        $deltamem > 10 ? sprintf("%.2fMB", $memory/1024)     : '------',
-                        $deltamem > 10 ? sprintf("%.2fMB", $deltamem/1024) : '------',
-                        $msg,
-                        $callfile,
-                        $caller[2],
-        );
+    my($msg)     = @_;
+    my @caller   = caller;
+    my $tmp      = [gettimeofday];
+    my $total    = tv_interval($starttime);
+    my $elapsed  = tv_interval($lasttime);
+    my $memory   = '';
+    my $deltamem = '';
+    if($has_memory) {
+        my $status   = read_file('/proc/'.$$.'/status');
+        $status     =~ m/^VmRSS:\s*(\d+)\s+kB/smxo;
+        $memory     = $1;
+        $deltamem   = $memory - $lastmemory;
+        $lastmemory = $memory;
     }
+    my $thr    = 'global';
+    if($has_threads) {
+        $thr = (threads->tid()||'global');
+    }
+    my $callfile = $caller[1];
+    $callfile =~ s|^.*lib/||mxo;
+    printf(STDERR "%-8s  %7s    %7s    %7s   %8s    %-50s %s:%d\n",
+                    $thr,
+                    $elapsed > 0.001 ? sprintf("%.1fs", $total)        : '------',
+                    $elapsed > 0.001 ? sprintf("%.3fs", $elapsed)      : '------',
+                    ($deltamem > 10 || $deltamem < -10) ? sprintf("%.1fMB", $memory/1024)   : '------',
+                    ($deltamem > 10 || $deltamem < -10) ? sprintf("%.2fMB", $deltamem/1024) : '------',
+                    $msg,
+                    $callfile,
+                    $caller[2],
+    );
     $lasttime = $tmp;
     return $elapsed;
 }
