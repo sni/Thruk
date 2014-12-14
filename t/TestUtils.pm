97258c6670cbc397ae0f733a67e7eea5ab6a50d1
Sven Nierlein <sven@nierlein.de>
2014-12-14 16:39:12 +0100

    test for script sources too

--- t/TestUtils.pm
+++ t/TestUtils.pm
@@ -348,7 +348,8 @@ sub test_page {
         my $content = $return->{'content'};
         # check for failed javascript lists
         verify_html_js($content) unless $opts->{'skip_js_check'};
-        $content =~ s/<script[^>]*>.*?<\/script>//gsmxio;
+        # remove script tags without a src
+        $content =~ s/<script[^>]*>\S+<\/script>//gsmxio;
         my @matches1 = $content =~ m/\s+(src|href)='(.+?)'/gio;
         my @matches2 = $content =~ m/\s+(src|href)="(.+?)"/gio;
         my $links_to_check;
