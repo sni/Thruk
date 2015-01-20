5dddf1b883a90ec55ef799621953318f61b187ca
Sven Nierlein <sven@nierlein.de>
2015-01-21 00:29:15 +0100

    fix icon logo path in statusmap parent view (fixes #438)

--- plugins/plugins-available/statusmap/root/statusmap.js
+++ plugins/plugins-available/statusmap/root/statusmap.js
@@ -1,8 +1,8 @@
 /* create tool tip content */
 function makeHTMLFromData(name, data){
   var html = '';
-  if (data.icon_image != undefined) {
-    html += '<img class="tip-icon" src="../../check_mk/images/icons/' + data.icon_image + '" />';
+  if (data.icon_image != undefined && data.icon_image != "") {
+    html += '<img class="tip-icon" src="'+url_prefix+'themes/'+theme+'/images/logos/'+data.icon_image+'" />';
   }
   if(data.status != undefined) {
     // real host leaf
