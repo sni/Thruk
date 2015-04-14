9cce878afbbf8a79291e45fa2283881a4e956574
Sven Nierlein <sven@nierlein.de>
2015-04-14 17:54:21 +0200

    conf: skip git check if config folder does not exist

--- plugins/plugins-available/conf/lib/Thruk/Controller/conf.pm
+++ plugins/plugins-available/conf/lib/Thruk/Controller/conf.pm
@@ -954,15 +954,14 @@ sub _process_objects_page {
     # check if we have a history for our configs
     my $files_root = $self->_set_files_stash($c);
     my $dir        = $c->{'obj_db'}->{'config'}->{'git_base_dir'} || $c->config->{'Thruk::Plugin::ConfigTool'}->{'git_base_dir'} || $files_root;
-    {
+    if(-d $dir) {
         local $SIG{CHLD} = 'DEFAULT';
         my $cmd          = "cd '".$dir."' && git log --pretty='format:%H' -1 2>&1";
         my $out          = `$cmd`;
         $c->stash->{'has_history'} = 1 if $? == 0;
         $c->log->debug($cmd);
         $c->log->debug($out);
-    };
-
+    }
 
     # apply changes?
     if(defined $c->{'request'}->{'parameters'}->{'apply'}) {
