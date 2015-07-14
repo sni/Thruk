0202d06a4d146ebcfb483689e3b05d62691a8e09
Sven Nierlein <Sven.Nierlein@consol.de>
2015-07-14 13:37:27 +0200

    limit fcgi procs to 20 by default

--- support/fcgid_env.sh
+++ support/fcgid_env.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-
 # set thruk environment
-export THRUK_CONFIG="/etc/thruk"
+# and run fastcgi server
 
-# load extra environment variables
-if [ -f /etc/sysconfig/thruk ]; then
-  . /etc/sysconfig/thruk
-fi
-if [ -f /etc/default/thruk ]; then
-  . /etc/default/thruk
-fi
-[ -e ~/.thruk ] && . ~/.thruk
+THRUK_MAX_FCGI_PROCS=20
 
-# execute fastcgi server
-exec /usr/share/thruk/script/thruk_fastcgi.pl
+if [ "x$OMD_ROOT" != "x" ]; then
+  export THRUK_CONFIG="$OMD_ROOT/etc/thruk"
+  THRUK_FCGI_BIN="/share/thruk/script/thruk_fastcgi.pl"
+  [ -e $OMD_ROOT/.profile ] && . $OMD_ROOT/.profile
+  [ -e $OMD_ROOT/.thruk   ] && . $OMD_ROOT/.thruk
+else
+  export THRUK_CONFIG="/etc/thruk"
+  THRUK_FCGI_BIN="/usr/share/thruk/script/thruk_fastcgi.pl"
+  [ -e /etc/sysconfig/thruk ] && . /etc/sysconfig/thruk
+  [ -e /etc/default/thruk ]   && . /etc/default/thruk
+  [ -e ~/.thruk ]             && . ~/.thruk
+fi
+
+# limit to 20 processes
+if [ $(ps -fu $(whoami) | grep -v grep | grep -c "$THRUK_FCGI_BIN") -ge $THRUK_MAX_FCGI_PROCS ]; then
+    echo "ERROR: limit of $THRUK_MAX_FCGI_PROCS fcgi procs reached, not starting another one." >&2
+    exit 1
+fi
+
+exec $THRUK_FCGI_BIN
