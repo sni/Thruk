17e3b05a6b1f2e96cba601772ca99b40e92d240b
Sven Nierlein <sven@nierlein.de>
2015-07-24 10:43:36 +0200

    make button use GET parameters optionally

--- lib/Thruk/Utils/Filter.pm
+++ lib/Thruk/Utils/Filter.pm
@@ -864,16 +864,22 @@ sub has_business_process {
 
 =head2 button
 
-  my $html = button($link, $value, $class, [$onclick])
+  my $html = button($link, $value, $class, [$onclick], [$formstyle], [$keeplink])
 
 returns button html source
 
 =cut
 sub button {
-    my($link, $value, $class, $onclick, $formstyle) = @_;
+    my($link, $value, $class, $onclick, $formstyle, $keeplink) = @_;
 
-    my($page, $args) = split(/\?/mx, $link, 2);
-    $args =~ s/&amp;/&/gmx;
+    my($page, $args);
+    if($keeplink) {
+        $page = $link;
+        $args = "";
+    } else {
+        ($page, $args) = split(/\?/mx, $link, 2);
+        $args =~ s/&amp;/&/gmx;
+    }
 
     my $html = '<form action="'.$page.'" method="POST"'.($formstyle ? 'style="'.$formstyle.'"' : '').'>';
     for my $a (split/\&/mx, $args) {
