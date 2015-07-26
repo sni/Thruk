511ab8320891a6c2b0f95ab42b18ae937116d343
Jean-François Rameau <jf.rameau@gmail.com>
2015-07-26 20:12:53 +0200

    Use host_action_icon instead of hardcoded action.gif image.

--- plugins/plugins-available/panorama/lib/Thruk/Controller/panorama.pm
+++ plugins/plugins-available/panorama/lib/Thruk/Controller/panorama.pm
@@ -1697,7 +1697,7 @@ sub _task_servicesminemap {
         my $hst  = $hosts->{$name};
         my $data;
         if ($hst->{'host_action_url_expanded'}) {
-            $data = { 'host_display_name' => $hst->{'host_display_name'} . '&nbsp;<a target="_blank" href="'.$hst->{'host_action_url_expanded'}.'"><img src="'.$c->stash->{'url_prefix'}.'themes/'.$c->stash->{'theme'}.'/images/action.gif" border="0" width="20" height="20" alt="Perform Extra Host Actions" title="Perform Extra Host Actions" style="vertical-align: text-bottom;"></a>' };
+            $data = { 'host_display_name' => $hst->{'host_display_name'} . '&nbsp;<a target="_blank" href="'.$hst->{'host_action_url_expanded'}.'"><img src="'.$c->stash->{'url_prefix'}.'themes/'.$c->stash->{'theme'}.'/images/'.$c->stash->{'host_action_icon'}.'" border="0" width="20" height="20" alt="Perform Extra Host Actions" title="Perform Extra Host Actions" style="vertical-align: text-bottom;"></a>' };
         } else {
             $data = { 'host_display_name' => $hst->{'host_display_name'} };
         }
