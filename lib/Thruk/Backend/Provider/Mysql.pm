4601f3e8efa85a352ace068f6dc188ad555cf61d
Jelle Smet <development@smetj.net>
2015-10-09 10:51:36 +0200

    Remove second my

--- lib/Thruk/Backend/Provider/Mysql.pm
+++ lib/Thruk/Backend/Provider/Mysql.pm
@@ -400,7 +400,7 @@ sub get_logs {
     # logfiles into tmp file
     my($fh, $filename);
     if($options{'file'}) {
-        my($fh, $filename) = tempfile(DIR => ($c->config->{'tmp_path'} || '/tmp'));
+        ($fh, $filename) = tempfile(DIR => ($c->config->{'tmp_path'} || '/tmp'));
         open($fh, '>', $filename) or die('open '.$filename.' failed: '.$!);
     }
 
