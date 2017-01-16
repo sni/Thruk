0890d653735c0bad7021098e92cdb0219dad5edc
Sven Nierlein <sven@nierlein.de>
2017-01-16 13:36:27 +0100

    panorama: honor acknowledgement and downtime status shape color calculation

--- plugins/plugins-available/panorama/root/js/panorama_js_panlet_icon_widget_functions.js
+++ plugins/plugins-available/panorama/root/js/panorama_js_panlet_icon_widget_functions.js
@@ -577,6 +577,8 @@ TP.getShapeColor = function(type, panel, xdata, forceColor) {
     if(state == undefined) { state = panel.xdata.state; }
     if(xdata.appearance[type+"source"] == undefined) { xdata.appearance[type+"source"] = 'fixed'; }
     if(forceColor != undefined) { fillcolor = forceColor; }
+    else if(panel.acknowledged) { fillcolor = xdata.appearance[type+"color_ok"]; }
+    else if(panel.downtime)     { fillcolor = xdata.appearance[type+"color_ok"]; }
     else if(state == 0)         { fillcolor = xdata.appearance[type+"color_ok"]; }
     else if(state == 1)         { fillcolor = xdata.appearance[type+"color_warning"]; }
     else if(state == 2)         { fillcolor = xdata.appearance[type+"color_critical"]; }
