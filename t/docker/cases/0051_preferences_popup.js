4b5e26ae959e9b6a172f1a4418a24c723bf0ab93
Sven Nierlein <sven@nierlein.de>
2017-03-30 23:27:02 +0200

    sakuli: update test cases

--- t/docker/cases/0051_preferences_popup.js
+++ t/docker/cases/0051_preferences_popup.js
@@ -5,14 +5,14 @@ try {
     thruk_login();
     click(_link("Tactical Overview"));
 
-    click(_button("preferences"));
+    click(_submit("preferences"));
     isVisible(_span("/Update in \\d+ seconds/"));
     click(_button("stop"));
     isVisible(_span("This page will not refresh automatically"));
     mouseClickXY(200,150);
     isNotVisible(_span("This page will not refresh automatically"));
-    click(_imageSubmitButton("arrow_refresh.png"));
-    click(_button("preferences"));
+    click(_submit("/top_refresh_button/"));
+    click(_submit("preferences"));
     isVisible(_span("/Update in \\d+ seconds/"));
 
     testCase.endOfStep("preferences popup", 20);
