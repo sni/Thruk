57dc1255c7346c2e6dc2e6db177e11d607d1ade6
Sven Nierlein <sven@nierlein.de>
2017-05-18 21:31:06 +0200

    fix grafana graph export

--- script/grafana_export.sh
+++ script/grafana_export.sh
@@ -22,12 +22,12 @@ export PATH=$PATH:$DIR
 
 [ -z $PHANTOMJS ] && PHANTOMJS="phantomjs"
 
-EXTRAOPTIONS="--ssl-protocol=tlsv1 --web-security=no --ignore-ssl-errors=true"
+EXTRAOPTIONS="--ssl-protocol=tlsv1 --web-security=no --ignore-ssl-errors=true $PHANTOMJSOPTIONS"
 
 INPUT="$INPUT&from=$START&to=$END"
 
 rm -f $OUTPUT
-$PHANTOMJS $EXTRAOPTIONS "$DIR/html2pdf.js" "$INPUT" "$TEMPFILE.png" --width=$WIDTH --height=$HEIGHT $PHANTOMJSOPTIONS 2>&1
+$PHANTOMJS $EXTRAOPTIONS "$DIR/html2pdf.js" "$INPUT" "$TEMPFILE.png" --width=$WIDTH --height=$HEIGHT $PHANTOMJSSCRIPTOPTIONS 2>&1
 rc=$?
 
 if [ -s "$TEMPFILE.png" ]; then
