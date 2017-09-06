8771d3132ea6ba77847a01b79c5c2815cf376af2
Sven Nierlein <sven@nierlein.de>
2017-09-06 10:30:56 +0200

    hide unused button on comined page

--- lib/Thruk/Controller/status.pm
+++ lib/Thruk/Controller/status.pm
@@ -1017,6 +1017,7 @@ sub _process_combined_page {
     $c->stash->{hidetop}    = 1 unless $c->stash->{hidetop} ne '';
     $c->stash->{show_substyle_selector} = 0;
     $c->stash->{'show_column_select'}   = 1;
+    $c->stash->{show_top_pane}          = 0;
 
     my $view_mode = $c->req->parameters->{'view_mode'} || 'html';
 
