f7709756f47d718e1058fcd42c67c57adee186e8
Sven Nierlein <sven@nierlein.de>
2017-12-22 11:43:18 +0100

    fix pager on config page when using lmd (fixes #752)

--- lib/Thruk/Backend/Manager.pm
+++ lib/Thruk/Backend/Manager.pm
@@ -1387,7 +1387,7 @@ sub _do_on_peers {
     if(scalar keys %arg > 0) {
         if( $arg{'remove_duplicates'} ) {
             $data = $self->_remove_duplicates($data);
-            $totalsize = scalar @{$data};
+            $totalsize = scalar @{$data} unless $ENV{'THRUK_USE_LMD'};
             $must_resort = 1;
         }
 
