70f97ff103a82bdc95a0bb19ae0ce415924846ce
Michele Santuari <santuari@users.noreply.github.com>
2018-01-24 10:46:15 +0100

    fix downtime filter (#791)
    * fix downtime filter
    
    if host is defined, only services of such host must be
    considered
    
    * fix the original servicefilter

--- lib/Thruk/Utils/RecurringDowntimes.pm
+++ lib/Thruk/Utils/RecurringDowntimes.pm
@@ -88,8 +88,9 @@ sub get_downtimes_list {
     }
 
     # host and service filter
-    push @servicefilter, { -and => [ { description => $service }, { host_name => $host} ] } if $service;
-    push @hostfilter, { name => $host }                                                     if $host;
+    push @servicefilter, {  host_name => $host }     if $host;
+    push @servicefilter, { description => $service } if $service;
+    push @hostfilter, { name => $host }              if $host;
 
     my($hosts, $services, $hostgroups, $servicegroups) = ({},{},{},{});
     if($host || $service) {
