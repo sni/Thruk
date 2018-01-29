92b6487b67268ac18ddf90cf22c4a75c8b01c4c2
Sven Nierlein <sven@nierlein.de>
2018-01-29 08:51:07 +0100

    update version of M::L

--- lib/Monitoring/Livestatus.pm
+++ lib/Monitoring/Livestatus.pm
@@ -12,7 +12,7 @@ use Storable qw/dclone/;
 use Monitoring::Livestatus::INET qw//;
 use Monitoring::Livestatus::UNIX qw//;
 
-our $VERSION = '0.78';
+our $VERSION = '0.80';
 
 
 # list of allowed options
