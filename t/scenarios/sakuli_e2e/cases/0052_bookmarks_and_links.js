fb4032a30fda25ee44bf4859961115f4a3912f6b
Sven Nierlein <sven@nierlein.de>
2018-02-01 13:04:26 +0100

    sakuli: add bookmark test case

--- t/scenarios/sakuli_e2e/cases/0052_bookmarks_and_links.js
+++ t/scenarios/sakuli_e2e/cases/0052_bookmarks_and_links.js
@@ -3,8 +3,9 @@ _include('../_include.js');
 
 var $case = function() {
     thruk_login();
-    click(_link("Services"));
 
+    // set bookmark for services page
+    click(_link("Services"));
     click(_submit("/add_bookmark/"));
     isVisible(_textbox("newname"));
     _setValue(_textbox("newname"), "Test Bookmark");
@@ -20,13 +21,30 @@ var $case = function() {
 
     isNotVisible(_link("Test Bookmark"));
 
+
+    // check links popup
     click(_link("Problems"));
     click(_submit("/show_link/"));
     isVisible(_textbox("url_input"));
     _assertEqual("/^http:/", _getValue(_textbox("url_input")));
     _assertNotEqual("/&amp;/", _getValue(_textbox("url_input")));
 
-    testCase.endOfStep("bookmarks", 30);
+
+    // add another bookmark
+    click(_submit("/add_bookmark/"));
+    isVisible(_textbox("newname"));
+    _setValue(_textbox("newname"), "Test Bookmark");
+    click(_submit("add bookmark"));
+
+    isVisible(_link("Test Bookmark"));
+
+    click(_submit("/add_bookmark/"));
+    click(_link("Remove Bookmarks"));
+    click(_imageSubmitButton("delete"));
+    click(_submit("save changes"));
+
+    testCase.endOfStep("bookmarks", 60);
 };
 
 runTest($case);
+
