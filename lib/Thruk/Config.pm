6bf35463aa4a4e0e45faaa0595412392a542e28e
Sven Nierlein <sven@nierlein.de>
2018-02-07 22:49:44 +0100

    use local site apache for grafana export if possible

--- lib/Thruk/Config.pm
+++ lib/Thruk/Config.pm
@@ -1043,12 +1043,13 @@ sub _do_finalize_config {
     # enable OMD tweaks
     if($ENV{'OMD_ROOT'}) {
         my $site = $ENV{'OMD_SITE'};
+        my($siteport) = (`grep CONFIG_APACHE_TCP_PORT etc/omd/site.conf` =~ m/(\d+)/mx);
+        my($ssl)      = (`grep CONFIG_APACHE_MODE     etc/omd/site.conf` =~ m/'(\w+)'/mx);
+        my $proto     = $ssl eq 'ssl' ? 'https' : 'http';
+        $config->{'omd_local_site_url'} = sprintf("%s://%s:%d/%s", $proto, "127.0.0.1", $siteport, $site);
         # bypass system reverse proxy for restricted cgi for permormance and locking reasons
         if($config->{'cookie_auth_restricted_url'} =~ m|^https?://localhost/$site/thruk/cgi-bin/restricted.cgi$|mx) {
-            my($siteport) = (`grep CONFIG_APACHE_TCP_PORT etc/omd/site.conf` =~ m/(\d+)/mx);
-            my($ssl)      = (`grep CONFIG_APACHE_MODE     etc/omd/site.conf` =~ m/'(\w+)'/mx);
-            my $proto     = $ssl eq 'ssl' ? 'https' : 'http';
-            $config->{'cookie_auth_restricted_url'} = sprintf("%s://%s:%d/%s/thruk/cgi-bin/restricted.cgi", $proto, "127.0.0.1", $siteport, $site);
+            $config->{'cookie_auth_restricted_url'} = $config->{'omd_local_site_url'}.'/thruk/cgi-bin/restricted.cgi';
         }
     }
 
