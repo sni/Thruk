5f65b29f0d3950d6bbac6f24bf49c7a83010ece8
Sven Nierlein <sven@nierlein.de>
2018-05-24 11:08:40 +0200

    panorama: replace frontmatter variables in panorama too

--- plugins/plugins-available/panorama/root/js/panorama_js_functions.js
+++ plugins/plugins-available/panorama/root/js/panorama_js_functions.js
@@ -2125,6 +2125,7 @@ var TP = {
             var b   = broadcasts[x];
             var id  = "broadcast_"+b.basefile.replace(/[^0-9a-z]/g, "");
             var text = replace_macros(b.text, b.macros);
+            var text = replace_macros(replace_macros(b.text, b.frontmatter), b.macros)
             if(b.annotation) {
                 text = '<img src="'+url_prefix+'themes/'+theme+'/images/'+b.annotation+'.png" border="0" alt="warning" title="'+b.annotation+'" width="16" height="16" style="vertical-align: text-bottom; margin-right: 5px;">'+text;
             }
