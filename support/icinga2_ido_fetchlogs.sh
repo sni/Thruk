147039fc2d20286ebf9c2ee54a7469b5e08809ee
Sven Nierlein <sven@nierlein.de>
2018-06-28 16:08:44 +0200

    ido: support ido postgres

--- support/icinga2_ido_fetchlogs.sh
+++ support/icinga2_ido_fetchlogs.sh
@@ -13,5 +13,17 @@ if [ "$THRUK_LOGCACHE_LIMIT" ]; then
     LIMIT="LIMIT $THRUK_LOGCACHE_LIMIT"
 fi
 
+if [ "x$1" = "x" ] || ! test -f $(dirname $0)/icinga2_log_entries.$1; then
+  echo "usage: $0 <db type>"
+  echo "db type can be mysql or postgres"
+  exit 3
+fi
+
 export INSTANCE_ID LIMIT TIME_START TIME_END
-envsubst < $(dirname $0)/icinga2_log_entries.sql | mysql -u "$IDO_DB_USER" -h "$IDO_DB_HOST" -P "$IDO_DB_PORT" -p"$IDO_DB_PW" "$IDO_DB_NAME"
+
+if [ "$1" = "mysql" ]; then
+  envsubst < $(dirname $0)/icinga2_log_entries.mysql | mysql -u "$IDO_DB_USER" -h "$IDO_DB_HOST" -P "$IDO_DB_PORT" -p"$IDO_DB_PW" "$IDO_DB_NAME"
+fi
+if [ "$1" = "postgres" ]; then
+  envsubst < $(dirname $0)/icinga2_log_entries.postgres | PGPASSWORD="$IDO_DB_PW" psql -U "$IDO_DB_USER" -h "$IDO_DB_HOST" -p "$IDO_DB_PORT" "$IDO_DB_NAME"
+fi
