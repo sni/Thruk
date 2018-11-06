55de894fefc40f0e32ca78f695983e8e6e327d13
Sven Nierlein <Sven.Nierlein@consol.de>
2018-11-06 16:27:18 +0100

    release 2.24-2

--- lib/Thruk/Config.pm
+++ lib/Thruk/Config.pm
@@ -31,7 +31,7 @@ BEGIN {
 our $VERSION = '2.24';
 
 my $project_root = home('Thruk::Config') or confess('could not determine project_root: '.Dumper(\%INC));
-my $branch       = '';
+my $branch       = '2';
 my $gitbranch    = get_git_name($project_root);
 my $filebranch   = $branch || 1;
 if($branch) {
@@ -46,7 +46,7 @@ $ENV{'THRUK_SRC'} = 'UNKNOWN' unless defined $ENV{'THRUK_SRC'};
 our %config = ('name'                   => 'Thruk',
               'version'                => $VERSION,
               'branch'                 => $branch,
-              'released'               => 'October 26, 2018',
+              'released'               => 'November 06, 2018',
               'compression_format'     => 'gzip',
               'ENCODING'               => 'utf-8',
               'image_path'             => $project_root.'/root/thruk/images',
