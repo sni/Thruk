319f37cef9920506be74c31fae173999d765c7eb
Sven Nierlein <sven@nierlein.de>
2019-03-26 09:40:10 +0100

    krb scenario: switch to http

--- t/scenarios/auth_krb5/vnc/user.js
+++ t/scenarios/auth_krb5/vnc/user.js
@@ -1,4 +1,4 @@
 user_pref("network.negotiate-auth.trusted-uris", ".test.local");
-user_pref("browser.startup.homepage", "https://omd.test.local/demo/thruk/");
+user_pref("browser.startup.homepage", "http://omd.test.local/demo/thruk/");
 user_pref("browser.shell.checkDefaultBrowser", "false");
 user_pref("general.warnOnAboutConfig", "false");
