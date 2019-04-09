2a93bb2b4d86b374bcc76db974f38691a88199b4
Sven Nierlein <sven@nierlein.de>
2019-04-09 09:03:01 +0200

    set pseudo tty flag for docker-compose

--- t/scenarios/cli_api/test.sh
+++ t/scenarios/cli_api/test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker-compose exec --user root omd bash -ci "su - demo -c 'thruk -l'"
+FLAGS=""
+if [ ! -t 0 ]; then
+  FLAGS="-T"
+fi
+docker-compose exec $FLAGS --user root omd bash -ci "su - demo -c 'thruk -l'"
