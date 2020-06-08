91d7c595437fa80efde5a6d96739b48080f68957
Sven Nierlein <sven@nierlein.de>
2020-06-08 14:21:23 +0200

    fix escaping when reloading bookmarks

--- lib/Thruk/Utils/Menu.pm
+++ lib/Thruk/Utils/Menu.pm
@@ -365,7 +365,6 @@ sub _renew_navigation {
                         name => $item->[0],
                         href => $href,
                     };
-                    $item->{'href'} = Thruk::Utils::Filter::escape_ampersand($item->{'href'});
                     push @{$user_items}, [ $section, $item ];
                 }
             }
