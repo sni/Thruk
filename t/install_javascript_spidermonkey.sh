ed62cc00d0d72e7d9f5d0f3cf3a2b719e6844106
Sven Nierlein <sven@nierlein.de>
2020-07-21 14:42:27 +0200

    travis: install javascript modules

---
+++ t/install_javascript_spidermonkey.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if ! test -d ~/perl5; then
+    echo "script will install JavaScript-SpiderMonkey-0.25 into ~/perl5 but target folder could not be found"
+    exit 1
+fi
+
+set -e
+set -u
+set -x
+
+eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+cd ~/perl5/
+mkdir -p js
+cd js
+wget "https://ftp.mozilla.org/pub/spidermonkey/releases/1.6.0/js-1.60.tar.gz"
+tar zxf js-1.60.tar.gz
+cd js/src
+make -f Makefile.ref
+cd ../..
+
+wget "https://cpan.metacpan.org/authors/id/T/TB/TBUSCH/JavaScript-SpiderMonkey-0.25.tar.gz"
+tar zxf JavaScript-SpiderMonkey-0.25.tar.gz
+cd JavaScript-SpiderMonkey-0.25
+perl Makefile.PL
+make install
