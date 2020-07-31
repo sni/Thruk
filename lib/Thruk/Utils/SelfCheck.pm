1ac3b1c7a3fcc11766798e41b0c1b981f635c489
Sven Nierlein <sven@nierlein.de>
2020-07-31 14:05:54 +0200

    fix using undefined value in selfcheck

--- lib/Thruk/Utils/SelfCheck.pm
+++ lib/Thruk/Utils/SelfCheck.pm
@@ -319,8 +319,8 @@ sub _lmd_checks  {
 
     my $details = "LMD:\n";
     if($c->config->{'lmd_core_bin'} && $c->config->{'lmd_core_bin'} ne 'lmd') {
-        my $lmd_core_bin = glob($c->config->{'lmd_core_bin'});
-        if(! -x $lmd_core_bin) {
+        my($lmd_core_bin) = glob($c->config->{'lmd_core_bin'});
+        if(!$lmd_core_bin || ! -x $lmd_core_bin) {
             chomp(my $err = $!);
             $details .= sprintf("  - lmd binary %s not executable: %s\n", $c->config->{'lmd_core_bin'}, $err);
             return({sub => 'lmd', rc => 2, msg => "LMD CRITICAL", details => $details });
