7c3f16c71964af25767d8e8b70c7d4da80a734bf
Sven Nierlein <sven@nierlein.de>
2020-12-17 11:26:16 +0100

    support debug timer in scripts

--- script/disable_debug_timer.sh
+++ script/disable_debug_timer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $(find lib/ plugins/plugins-available/*/lib/ -type f); do
+for file in $(find script/ lib/ plugins/plugins-available/*/lib/ -type f | grep -v _debug_timer.sh); do
   if [ $file = "lib/Thruk/Timer.pm" ]; then continue; fi
   sed -i \
       -e 's/#*use Thruk::Timer/#use Thruk::Timer/g' \
