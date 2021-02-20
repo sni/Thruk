4bb4c949fa80b3aeb1c1b768a5595e47425da1ce
Sven Nierlein <sven@nierlein.de>
2021-02-20 16:54:52 +0100

    fix fatal: No names found, cannot describe anything

--- lib/Thruk/Config.pm
+++ lib/Thruk/Config.pm
@@ -866,10 +866,10 @@ sub _get_git_info {
         return $git_info;
     }
     if(!$hash) {
-        $hash = _cmd('cd '.$project_root.' && git log -1 --no-color --pretty=format:%h 2> /dev/null');
+        $hash = _cmd('cd '.$project_root.' && git log -1 --no-color --pretty=format:%h 2>/dev/null');
     }
 
-    my $commits = _cmd('cd '.$project_root.' && git log --oneline $(cd '.$project_root.' && git describe --tags --abbrev=0).. 2>/dev/null | wc -l');
+    my $commits = _cmd('cd '.$project_root.' && git log --oneline $(cd '.$project_root.' && git describe --tags --abbrev=0 2>/dev/null).. 2>/dev/null | wc -l');
 
     if($branch eq 'master') {
         $git_info = "+".$commits."~".$hash;
