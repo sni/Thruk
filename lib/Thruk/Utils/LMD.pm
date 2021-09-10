e3fc7237982d4a332d264570d4c25226042d254e
Sven Nierlein <sven@nierlein.de>
2021-09-10 09:32:38 +0200

    lmd: fix status check

--- lib/Thruk/Utils/LMD.pm
+++ lib/Thruk/Utils/LMD.pm
@@ -150,10 +150,11 @@ sub status {
     return($status, $total_started, $start_time) unless $config->{'use_lmd_core'};
 
     my $lmd_dir = $config->{'tmp_path'}.'/lmd';
-    if(-e $lmd_dir.'/live.sock' && ($pid = check_pid($lmd_dir.'/pid'))) {
+    my $pidfile = $lmd_dir.'/pid';
+    if(-e $lmd_dir.'/live.sock' && ($pid = check_pid($pidfile))) {
         $total_started++;
         $started = 1;
-        $start_time = (stat(_))[10];
+        $start_time = (stat($pidfile))[10];
     }
     push @{$status}, { status => $started, pid => $pid, start_time => $start_time };
     return($status, $total_started);
