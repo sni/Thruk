7b982b84bc3e9e426c31e6385fc97a4e619a5438
Sven Nierlein <sven@nierlein.de>
2021-12-30 15:09:30 +0100

    remove incompatible theme Vautour

--- themes/themes-available/Vautour/js/search.js
+++
@@ -1,9 +0,0 @@
-//window.addEvent('domready', function(){
-//	// Search
-//	var srch = $('search');
-//	srch.setProperty('value', 'Search ...');
-//	srch.addEvents({
-//		'focus': function() { srch.value = (srch.value == 'Search ...' ? '' : srch.value); },
-//		'blur': function()  { srch.value = (srch.value == '' ? 'Search ...' : srch.value); }
-//	});
-//});
