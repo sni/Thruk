713bd42592f7ab8f77cca5901f5ee906aacb4987
Sven Nierlein <sven@nierlein.de>
2022-06-03 11:32:55 +0200

    hide some parts on the error page if not logged in

--- lib/Thruk/Controller/error.pm
+++ lib/Thruk/Controller/error.pm
@@ -68,6 +68,7 @@ sub index {
         $c->stash->{'remote_user'}  = $c->user->get('username');
     } else {
         $c->stash->{'remote_user'}  = '?';
+        $c->stash->{'show_nav'}     = 0;
     }
 
     # status code must be != 200, otherwise compressed output will fail
