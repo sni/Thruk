99e0033bd1ac2275a44cf2502bfec95071341475
Sven Nierlein <Sven.Nierlein@consol.de>
2023-05-24 14:13:18 +0200

    release v3.06

--- lib/Thruk/Config.pm
+++ lib/Thruk/Config.pm
@@ -26,7 +26,7 @@ Generic Access to Thruks Config
 
 ######################################
 
-our $VERSION = '3.04';
+our $VERSION = '3.06';
 
 our $config;
 my $project_root = home() || confess('could not determine project_root from inc.');
@@ -34,7 +34,7 @@ my $project_root = home() || confess('could not determine project_root from inc.
 my $base_defaults = {
     'name'                                  => 'Thruk',
     'fileversion'                           => $VERSION,
-    'released'                              => 'February 20, 2023',
+    'released'                              => 'May 24, 2023',
     'compression_format'                    => 'gzip',
     'ENCODING'                              => 'utf-8',
     'image_path'                            => $project_root.'/root/thruk/images',
