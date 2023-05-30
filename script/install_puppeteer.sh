f71d2019dfa1254b4d93e3a5e625e52aaf3e8b0f
Sven Nierlein <Sven.Nierlein@consol.de>
2023-05-30 10:08:13 +0200

    improve puppeteer installation script

--- script/install_puppeteer.sh
+++ script/install_puppeteer.sh
@@ -5,16 +5,51 @@
 # installs puppeteer into /var/lib/thruk/puppeteer so it can be used to render PDFs
 #
 
+###########################################################
+# node version to use
+USE_NODE=16
+
+# target folder
 DEST=$1
 if [ -z "$DEST" ]; then
     DEST=/var/lib/thruk/puppeteer
 fi
 
+NPM="npm"
+INSTALL_NODE=0
+
+###########################################################
+# check requirements
+ARCH=$(uname -m)
+if [ $ARCH != "x86_64" -a $ARCH != "aarch64" ]; then
+    echo "ERROR: automatic puppeteer installation is only supported on arm64 and aarch64, you have: $(uname -m)"
+    exit 1
+fi
+
+if [ $(id -u) != "0" ]; then
+    echo "ERROR: automatic puppeteer installation requires root permissions."
+    exit 1
+fi
+
 type npm >/dev/null 2>&1 || (
-    echo "npm is required to install puppeteer"
+    echo "ERROR: npm is required to install puppeteer"
     exit 1
 )
 
+NODE_VERSION=$(node -v 2>/dev/null | sed -e 's/^v//' -e 's/\..*$//g')
+if [ -z $NODE_VERSION ]; then
+    echo "ERROR: failed to detect node version: $(node -v)"
+    exit 1
+fi
+
+if [ $NODE_VERSION -lt $USE_NODE ]; then
+    export N_PREFIX=$(pwd)/node
+    echo "system node version $(node -v) too old, installing $USE_NODE into $N_PREFIX"
+    INSTALL_NODE=1
+fi
+
+###########################################################
+# do installation
 set -e
 mkdir -p $DEST
 cd $DEST
@@ -22,27 +57,19 @@ export PUPPETEER_DOWNLOAD_PATH=$DEST/chromium
 echo "module.exports = {}" > .puppeteerrc.cjs
 mkdir -p node_modules
 
-# check node version, must be at least 16
-NODE_VERSION=$(node -v 2>/dev/null | sed -e 's/^v//' -e 's/\..*$//g')
-if [ -z $NODE_VERSION ]; then
-    echo "failed to detect node version"
-    exit 1
-fi
-
-NPM="npm"
-if [ $NODE_VERSION -lt 16 ]; then
+if [ $INSTALL_NODE = "1" ]; then
     npm i n
-    export N_PREFIX=$(pwd)/node
-    ./node_modules/.bin/n 16
-    NPM="./node_modules/.bin/n exec 16 npm"
+    ./node_modules/.bin/n $USE_NODE
+    NPM="./node_modules/.bin/n exec $USE_NODE npm"
 fi
 
 $NPM i progress puppeteer
 set +e
 
-if [ $NODE_VERSION -lt 16 ]; then
-    $NPM i n
-    ./node_modules/.bin/n 16
+if [ $INSTALL_NODE = "1" ]; then
+    # install again, somehow previous module install removes it
+    npm i n
+    ./node_modules/.bin/n $USE_NODE
 fi
 
 if [ -n "$PUPPETEER_SKIP_CHROMIUM_DOWNLOAD" ]; then
@@ -63,7 +90,7 @@ if [ -n "$MISSING" ]; then
 
     MISSING=$(ldd $DEST/chromium/chrome/*/chrome-*/chrome | grep "=> not found")
     if [ -n "$MISSING" ]; then
-        echo "chrome requires some libraries to work:"
+        echo "chrome requires some more libraries to work:"
         echo "$MISSING"
         exit 1
     fi
