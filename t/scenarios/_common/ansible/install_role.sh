2da9d1607f153505acbc70508c7d856a848d4a2b
Sven Nierlein <sven@consol.de>
2024-01-23 16:31:56 +0100

    make tests more reliable

--- t/scenarios/_common/ansible/install_role.sh
+++ t/scenarios/_common/ansible/install_role.sh
@@ -4,7 +4,7 @@ ROLE=$1
 shift
 
 export ANSIBLE_RETRY_FILES_ENABLED="False"
-ansible-playbook -i localhost, -c local -e SITENAME=demo "$@" /dev/stdin <<END
+ansible-playbook -f1 -i localhost, -c local -e SITENAME=demo "$@" /dev/stdin <<END
 ---
 - hosts: localhost
   roles:
