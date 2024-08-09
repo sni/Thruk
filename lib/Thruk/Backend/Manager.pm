0a14ab6b7cd8a630c87f5da45833692a3fb815ce
Sven Nierlein <sven@consol.de>
2024-08-09 17:28:56 +0200

    fix reports test

--- lib/Thruk/Backend/Manager.pm
+++ lib/Thruk/Backend/Manager.pm
@@ -2963,7 +2963,7 @@ sub _add_query_stats {
         $profile->{'debug_hint'} = $arg{'debug_hint'} if defined $arg{'debug_hint'};
     }
     if(!$profile->{'debug_hint'}) {
-        my $stack = $profile->{'stack'} = Carp::longmess($function);
+        my $stack = $profile->{'stack'} || Carp::longmess($function);
         if($stack =~ m/lib\/Thruk\/Controller\/(\S+\.pm)\ line\ (\d+)/mx) {
             $profile->{'debug_hint'} = sprintf("%s:%s", $1, $2);
         }
