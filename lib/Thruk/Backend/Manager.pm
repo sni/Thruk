01d1ed3cbcb5e4d69f7ad4103af501be7247b85b
Sven Nierlein <sven@consol.de>
2024-10-24 13:51:38 +0200

    debug: show trace for queries

--- lib/Thruk/Backend/Manager.pm
+++ lib/Thruk/Backend/Manager.pm
@@ -3031,8 +3031,12 @@ sub _add_query_stats {
         $profile->{'debug_hint'} = $arg{'debug_hint'} if defined $arg{'debug_hint'};
     }
     if(!$profile->{'debug_hint'}) {
-        my $stack = $profile->{'stack'} || Carp::longmess($function);
+        $profile->{'stack'} ||= Carp::longmess($function);
+        my $stack = $profile->{'stack'};
+        $profile->{'debug_hint'} = 'stack';
         if($stack =~ m/lib\/Thruk\/Controller\/(\S+\.pm)\ line\ (\d+)/mx) {
+            $profile->{'debug_hint'} = sprintf("%s:%s", $1, $2);
+        } elsif($stack =~ m/\/?(\S+)\ line\ (\d+)/mx) {
             $profile->{'debug_hint'} = sprintf("%s:%s", $1, $2);
         }
     }
