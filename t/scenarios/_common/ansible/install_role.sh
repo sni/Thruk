7f5b813b1e09a36d366737c8ef44cc80c9c7928a
Sven Nierlein <sven@consol.de>
2025-02-03 13:39:25 +0100

    ansible: add missing variable

--- t/scenarios/_common/ansible/install_role.sh
+++ t/scenarios/_common/ansible/install_role.sh
@@ -7,6 +7,8 @@ export ANSIBLE_RETRY_FILES_ENABLED="False"
 ansible-playbook -f1 -i localhost, -c local -e SITENAME=demo "$@" /dev/stdin <<END
 ---
 - hosts: localhost
+  vars:
+    site: demo
   roles:
     - $ROLE
 END
